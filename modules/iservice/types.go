04a40e3156c8a0410c2a13838782000709c647fc
zhangzhiqiang <745124335@aa.com>
2018-03-09 10:11:23 +0800

    modify service bind

--- modules/iservice/types.go
+++ modules/iservice/types.go
@@ -12,19 +12,19 @@ func defaultParams() Params {
 }
 
 //_________________________________________________________________________
-
-type ServiceDefinition struct{
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Tags        string `json:"tags"` // ('k1'='v1','k2'='v2')
-	Creator     string `json:"creator"`
-	ChainID     string `json:"chainID"`
-	Messaging   string `json:"messaging"`
+//服务定义
+type ServiceDefinition struct {
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	Tags        string          `json:"tags"` // ('k1'='v1','k2'='v2')
+	Creator     string          `json:"creator"`
+	ChainID     string          `json:"chainID"`
+	Messaging   string          `json:"messaging"`
 	Methods     []ServiceMethod `json:"methods"`
 }
 
 type ServiceMethod struct {
-	ID            int64 `json:"id"`
+	ID            int    `json:"id"`
 	Name          string `json:"name"`
 	Description   string `json:"description"`
 	Input         string `json:"input"`
@@ -32,3 +32,29 @@ type ServiceMethod struct {
 	Error         string `json:"error"`
 	OutputPrivacy string `json:"outputPrivacy"`
 }
+
+//服务绑定
+type ServiceBind struct {
+	DefinitionHash    []byte `json:"definitionHash"`
+	ChainID           string `json:"chainID"`
+	ProviderAddress   []byte `json:"providerAddress"`
+	BindingType       string `json:"bindingType"`
+	ProviderDeposit   int64  `json:"providerDeposit"`
+	ServicePricing    string `json:"servicePricing"`
+	ServiceLevel      string `json:"serviceLevel"`
+	BindingExpiration int64  `json:"bindingExpiration"`
+	IsValid           bool   `json:"isValid"`
+}
+
+//服务调用
+type ServiceRequest struct {
+	ChainID         string `json:"chainID"`
+	ConsumerAddress []byte `json:"consumerAddress"`
+	DefinitionHash  []byte `json:"definitionHash"`
+	MethodID        int    `json:"methodID"`
+	InputValue      string `json:"inputValue"`
+	BindingHash     []byte `json:"bindingHash"`
+	MaxServiceFee   int64  `json:"maxServiceFee"`
+	Timeout         int64  `json:"timeout"`
+}
+
