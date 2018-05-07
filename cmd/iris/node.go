810b52b0dbd23bba35cd1b4645dd87fbde2e1f7a
zhangzhiqiang <745124335@aa.com>
2018-05-07 15:48:49 +0800

    IRISHUB-28:基于cosmos-sdk[0.14.1]开发iservice的服务定义部分的demo

--- cmd/iris/node.go
+++ cmd/iris/node.go
@@ -17,7 +17,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/stack"
 	"github.com/cosmos/cosmos-sdk/state"
 
-	"github.com/cosmos/gaia/modules/stake"
+	"github.com/MrXJC/gaia/modules/stake"
 )
 
 // nodeCmd is the entry point for this binary
