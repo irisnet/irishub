41b1ab487a748d459990a00f518faa455fed56f8
zhangyelong <yelong@bianjie.ai>
2018-06-21 00:01:23 +0800

    #DEVOPS-64 fix start.sh for sandbox

--- testnets/develop/start.sh
+++ testnets/develop/start.sh
@@ -5,19 +5,16 @@ curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/node_key.json -o ~/.iris/config/node_key.json
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/priv_validator.json -o ~/.iris/config/priv_validator.json
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/gentx/gentx-3b11af84e5cc261178eaa328978b0dd61adc561a.json -o ~/.iris/config/gentx/gentx-3b11af84e5cc261178eaa328978b0dd61adc561a.json
-iris init --gen-txs --chain-id=fuxi-develop
+iris init --chain-id=fuxi-develop --name=init1
 SP=$(curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/seed_phrase)
 command="iriscli keys add init1 --recover"
 expect -c "
     spawn $command;
     expect {
-        \"override the existing name*\" {send \"y\r\";
-        expect {
-           \"Enter a passphrase for your key:\" {send \"1234567890\r\";
-           expect {
-               \"Repeat the passphrase:\" {send \"1234567890\r\"; expect {
-                 \"Enter your recovery seed phrase:\" {send \"$SP\r\"; exp_continue}
-               }}}
-           }}}
-        }"
-#iris start
+        \"override the existing name*\" {send \"y\r\"; exp_continue}
+        \"Enter a passphrase for your key:\" {send \"1234567890\r\"; exp_continue}
+        \"Repeat the passphrase:\" {send \"1234567890\r\"; exp_continue}
+        \"Enter your recovery seed phrase:\" {send \"$SP\r\"; exp_continue}
+    }
+"
+iris start
