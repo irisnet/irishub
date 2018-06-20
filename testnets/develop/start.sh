99f106076a5dce263acb2e3daea0d3906182048c
zhangyelong <yelong@bianjie.ai>
2018-06-20 23:36:49 +0800

    #DEVOPS-64 update config for sandbox

--- testnets/develop/start.sh
+++ testnets/develop/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 rm -rf ~/.iris*
-iris init gen-tx --chain-id=fuxi-develop --name=init1
+mkdir -p ~/.iris/config/gentx/
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/genesis.json -o ~/.iris/config/genesis.json
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/node_key.json -o ~/.iris/config/node_key.json
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/priv_validator.json -o ~/.iris/config/priv_validator.json
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/gentx/gentx-3b11af84e5cc261178eaa328978b0dd61adc561a.json -o ~/.iris/config/gentx/gentx-3b11af84e5cc261178eaa328978b0dd61adc561a.json
+iris init --gen-txs --chain-id=fuxi-develop
 SP=$(curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/seed_phrase)
 command="iriscli keys add init1 --recover"
 expect -c "
@@ -18,9 +19,5 @@ expect -c "
                  \"Enter your recovery seed phrase:\" {send \"$SP\r\"; exp_continue}
                }}}
            }}}
-        }}
-
-    };
-
-"
-iris start
+        }"
+#iris start
