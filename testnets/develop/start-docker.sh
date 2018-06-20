889faa943ccb378c531846315e75e176f04e798b
zhangyelong <yelong@bianjie.ai>
2018-06-21 00:25:23 +0800

    #DEVOPS-64 update start-docker.sh & add README file

--- testnets/develop/start-docker.sh
+++ testnets/develop/start-docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-docker run -d -p 46656:46656 -p 46657:46657 -it irishub:develop sh -c '
-    iris init --chain-id=fuxi-develop --name=init1 && \
-    apk add --no-cache curl && \
-    curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/genesis.json -o ~/.iris/config/genesis.json && \
-
-    iris start'
+docker run -d -p 46656:46656 -p 46657:46657 --name irishub-sandbox -it irishub:develop sh -c '
+    apk add --no-cache curl expect && \
+    curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/start.sh -o start.sh && \
+    sh start.sh'
