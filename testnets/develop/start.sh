8cf23735ffb53e6ecf7d3846cece57044c61c761
zhangyelong <yelong@bianjie.ai>
2018-06-20 23:14:14 +0800

    #DEVOPS-64 update config for sandbox

--- testnets/develop/start.sh
+++ testnets/develop/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf ~/.iris*
 iris init --chain-id=fuxi-develop --name=init1
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/genesis.json -o ~/.iris/config/genesis.json
 curl https://raw.githubusercontent.com/irisnet/irishub/develop/testnets/develop/config/node_key.json -o ~/.iris/config/node_key.json
@@ -8,10 +9,17 @@ command="iriscli keys add init1 --recover"
 expect -c "
     spawn $command;
     expect {
-            \"override the existing name init1 [y/n]:\" {send \"y\r\"; exp_continue},
-            \"Enter a passphrase for your key:\" {send \"1234567890\r\"; exp_continue},
-            \"Repeat the passphrase:\" {send \"1234567890\r\"; exp_continue},
-            \"Enter your recovery seed phrase:\" {send \"$SP\r\"; exp_continue}
-            }
-        "
+        \"override the existing name*\" {send \"y\r\";
+        expect {
+           \"Enter a passphrase for your key:\" {send \"1234567890\r\";
+           expect {
+               \"Repeat the passphrase:\" {send \"1234567890\r\"; expect {
+                 \"Enter your recovery seed phrase:\" {send \"$SP\r\"; exp_continue}
+               }}}
+           }}}
+        }}
+
+    };
+
+"
 iris start
