3235baa5737dbe71b371e75ce431b3a220ab031b
zhangzhiqiang <745124335@aa.com>
2018-06-24 13:17:28 +0800

    Add offline signature API

--- cmd/iriscli/offline_sign.go
+++ cmd/iriscli/offline_sign.go
@@ -8,6 +8,9 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"io/ioutil"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/tendermint/go-crypto"
 )
 
 
@@ -15,28 +18,51 @@ func RegisterRoutes(ctx context.CoreContext, r *mux.Router, cdc *wire.Codec, kb 
 	r.HandleFunc("/tx/send", SendTxRequestHandlerFn(cdc, kb, ctx)).Methods("POST")
 }
 
-// /accounts/{address}
-type sendTxReq struct {
-	tx []byte
+
+type sendTx struct {
+	Msg        bank.MsgSend    `json:"msg"`
+	Fee        auth.StdFee     `json:"fee"`
+	Signatures []StdSignature  `json:"signatures"`
+}
+
+type StdSignature struct {
+	PubKey    		 crypto.PubKeyEd25519	`json:"pub_key"` // optional
+	Signature 		 crypto.SignatureEd25519	`json:"signature"`
+	AccountNumber    int64 		`json:"account_number"`
+	Sequence         int64 		`json:"sequence"`
 }
 
 //send traction(sign with rainbow) to irishub
 func SendTxRequestHandlerFn(cdc *wire.Codec, kb keys.Keybase, ctx context.CoreContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var sendReq sendTxReq
+		var tx sendTx
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 			return
 		}
-		if err = json.Unmarshal(body, &sendReq); err != nil {
+
+		if err = json.Unmarshal(body, &tx); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 			return
 		}
+		var sig = make([]auth.StdSignature,len(tx.Signatures))
+		for index,s := range tx.Signatures {
+			sig[index].PubKey = s.PubKey
+			sig[index].Signature = s.Signature
+			sig[index].AccountNumber =s.AccountNumber
+			sig[index].Sequence = s.Sequence
+		}
+		var stdTx = auth.StdTx{
+			Msg:tx.Msg,
+			Fee:tx.Fee,
+			Signatures:sig,
+		}
+		txByte,_ := cdc.MarshalBinary(stdTx)
 		// send
-		res, err := ctx.BroadcastTx(sendReq.tx)
+		res, err := ctx.BroadcastTx(txByte)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
