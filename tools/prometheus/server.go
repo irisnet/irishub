f05af8fba9e92000f2c94d1d9e5246f700a5a661
programokey <872127164@qq.com>
2018-07-09 02:33:12 -0700

    add monitor terms of disk usage, and support monitoring directories and files

--- tools/prometheus/server.go
+++ tools/prometheus/server.go
@@ -9,7 +9,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/spf13/viper"
 	"strings"
-	"fmt"
 	"github.com/irisnet/irishub/tools"
 )
 
@@ -32,20 +31,28 @@ func MonitorCommand(storeName string, cdc *wire.Codec) *cobra.Command {
 
 			paths := viper.GetString("paths")
 			commands := viper.GetString("commands")
+			disks := viper.GetString("disks")
 
 			for _, command := range strings.Split(commands, ";"){
 				if strings.TrimSpace(command) != ""{
-					fmt.Println("command:", strings.TrimSpace(command))
 					sysMetrics.AddProcess(strings.TrimSpace(command))
+				}
+			}
+
+			for _, disk_path := range strings.Split(disks, ";"){
+				if strings.TrimSpace(disk_path) != ""{
+					sysMetrics.AddDisk(strings.TrimSpace(disk_path))
 				}
 			}
 
 			for _, path := range strings.Split(paths, ";"){
 				if strings.TrimSpace(path) != ""{
-					fmt.Println("path:", strings.TrimSpace(path))
-					sysMetrics.AddDirectory(strings.TrimSpace(path))
+					sysMetrics.AddPath(strings.TrimSpace(path))
 				}
 			}
+
+			recursively := viper.GetBool("recursively")
+			sysMetrics.SetRecursively(recursively)
 
 			sysMetrics.Monitor()
 
@@ -72,7 +79,12 @@ func MonitorCommand(storeName string, cdc *wire.Codec) *cobra.Command {
 	cmd.Flags().StringP("commands", "c", "iris start", `the processes you want to monitor that started 
 by these commands, separated by semicolons ';'. 
 eg: --commands="command 0;command 1;command 2", --commands=iris by default`)
-	cmd.Flags().StringP("paths", "p", "/", `directories for config and data, separated by semicolons ';'. 
-eg: --paths="/;/mnt1;/mnt2"`)
+	cmd.Flags().StringP("disks", "d", "/", `mounted paths of storage devices, separated by semicolons ';'. 
+eg: --disks="/;/mnt1;/mnt2"`)
+	cmd.Flags().StringP("paths", "p", "", `path to config and data files/directories, separated by semicolons ';'.
+cannot use ~ and environment variables. eg: --paths="/etc;/home;
+size of files in sub-directories is excluded. to compute the size recursively, you can use --recursively=true"`)
+	cmd.Flags().BoolP("recursively", "r", false, `specify whether the files in sub-directories is included, 
+excluded by default. If there are many files & sub-directory in given directories, this program may be very slow!`)
 	return cmd
 }
