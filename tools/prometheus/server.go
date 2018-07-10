459121f200487b2deb1cf98549b35ff35f170631
zhangzhiqiang <745124335@aa.com>
2018-07-10 18:46:43 +0800

    Code optimization

--- tools/prometheus/server.go
+++ tools/prometheus/server.go
@@ -8,6 +8,7 @@ import (
 	cmn "github.com/tendermint/tmlibs/common"
 	"log"
 	"net/http"
+	"github.com/irisnet/irishub/app"
 )
 
 func MonitorCommand(storeName string, cdc *wire.Codec) *cobra.Command {
@@ -16,17 +17,9 @@ func MonitorCommand(storeName string, cdc *wire.Codec) *cobra.Command {
 		Short: "irishub monitor",
 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			csMetrics, p2pMetrics, memMetrics, sysMetrics := DefaultMetricsProvider()
-			ctx := tools.NewContext()
-
-			//监控共识指标
-			csMetrics.Monitor(ctx, cdc, storeName)
-			//监控p2p指标
-			p2pMetrics.Monitor(ctx)
-			//监控mempool指标
-			memMetrics.Monitor(ctx)
-			//监控系统指标
-			sysMetrics.Monitor()
+			ctx := tools.NewContext(storeName,cdc)
+			monitor := DefaultMonitor(ctx)
+			monitor.Start()
 
 			srv := &http.Server{
 				Addr:    ":26660",
@@ -53,7 +46,7 @@ by these commands, separated by semicolons ';'.
 eg: --commands="command 0;command 1;command 2", --commands=iris by default`)
 	cmd.Flags().StringP("disks", "d", "/", `mounted paths of storage devices, separated by semicolons ';'. 
 eg: --disks="/;/mnt1;/mnt2"`)
-	cmd.Flags().StringP("paths", "p", "~/.iris", `path to config and data files/directories, separated by semicolons ';'.
+	cmd.Flags().StringP("paths", "p", app.DefaultNodeHome, `path to config and data files/directories, separated by semicolons ';'.
 cannot use ~ and environment variables. eg: --paths="/etc;/home;
 size of files in sub-directories is excluded. to compute the size recursively, you can use --recursively=true"`)
 	cmd.Flags().BoolP("recursively", "r", false, `specify whether the files in sub-directories is included, 
