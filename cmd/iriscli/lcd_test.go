bef0b057cf448f9a77bf6d1a98c31ffdf37297c1
zhangzhiqiang <745124335@aa.com>
2018-07-20 16:58:43 +0800

    IRISHUB-47:调整交易api(/tx/send)

--- cmd/iriscli/lcd_test.go
+++ cmd/iriscli/lcd_test.go
@@ -5,12 +5,15 @@ import (
 	"github.com/irisnet/irishub/tools/prometheus"
 	"github.com/spf13/viper"
 	"testing"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"encoding/json"
+	"fmt"
 )
 
 func TestRestServer(t *testing.T) {
 	cdc := app.MakeCodec()
 	comm := ServeCommand(cdc)
-	viper.Set("chain-id", "fuxi")
+	viper.Set("chain-id", "fuxi-1001")
 	viper.Set("node", "tcp://localhost:26657")
 	viper.Set("laddr", "tcp://localhost:1317")
 
@@ -22,4 +25,10 @@ func TestMetricsCmd(t *testing.T) {
 	comm := prometheus.MonitorCommand(cdc)
 	viper.Set("node", "tcp://0.0.0.0:26657")
 	comm.ExecuteC()
+}
+
+func TestCoins(t *testing.T) {
+	someCoins := sdk.Coins{sdk.NewCoin("atom", 123)}
+	bz,_ := json.Marshal(someCoins)
+	fmt.Println(string(bz))
 }
