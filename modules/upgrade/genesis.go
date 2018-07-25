4e23de67784440cecc5a73faa3aed541fb72ccb5
xujiacheng <xujiacheng28@outlook.com>
2018-07-25 17:00:17 +0800

    change ModulesLifeTime-> ModuleLifeTimeList

---
+++ modules/upgrade/genesis.go
@@ -0,0 +1,9 @@
+package upgrade
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// InitGenesis - store genesis parameters For first Version
+func InitGenesis(ctx sdk.Context, k Keeper) {
+
+}
