ee6b4aad50b78b27668c00de262307459924c5c6
chengsilei <chengsilei@gmail.com>
2018-08-06 15:39:28 +0800

    do switch by the module list bucket

--- modules/upgrade/genesis.go
+++ modules/upgrade/genesis.go
@@ -3,7 +3,6 @@ package upgrade
 import (
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"strings"
 )
 
 // InitGenesis - build the genesis version For first Version
@@ -11,16 +10,7 @@ func InitGenesis(ctx sdk.Context, k Keeper, router bam.Router) {
 
 	RegisterModuleList(router)
 
-	modulelist := NewModuleLifeTimeList()
-	handlerList := router.RouteTable()
-
-	for _, handler := range handlerList {
-		hs := strings.Split(handler, "/")
-		stores := strings.Split(hs[1], ":")
-		modulelist = modulelist.BuildModuleLifeTime(0, hs[0], stores)
-	}
-
-	genesisVersion := NewVersion(0, 0, 0, modulelist)
+	genesisVersion := NewVersion(0, 0, 0, GetModuleListFromBucket(0))
 	k.AddNewVersion(ctx, genesisVersion)
 
 	InitGenesis_commitID(ctx, k)
