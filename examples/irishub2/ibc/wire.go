730ac29766dd1e1a7441d8a310b641c4ace70f68
Raymond <chengsilei@gmail.com>
2018-08-14 11:48:05 +0800

    update upgrade example && test plan doc

--- examples/irishub2/ibc/wire.go
+++ examples/irishub2/ibc/wire.go
@@ -6,8 +6,8 @@ import (
 
 // Register concrete types on wire codec
 func RegisterWire(cdc *wire.Codec) {
-	cdc.RegisterConcrete(IBCTransferMsg{}, "cosmos-sdk/IBCTransferMsg", nil)
-	cdc.RegisterConcrete(IBCReceiveMsg{}, "cosmos-sdk/IBCReceiveMsg", nil)
-	cdc.RegisterConcrete(IBCSetMsg{},"cosmos-sdk/IBCSetMsg",nil)
-	cdc.RegisterConcrete(IBCGetMsg{},"cosmos-sdk/IBCGetMsg",nil)
+	cdc.RegisterConcrete(IBCTransferMsg{}, "cosmos-sdk/IBCTransferMsg/2", nil)
+	cdc.RegisterConcrete(IBCReceiveMsg{}, "cosmos-sdk/IBCReceiveMsg/2", nil)
+	cdc.RegisterConcrete(IBCSetMsg{},"cosmos-sdk/IBCSetMsg/2",nil)
+	cdc.RegisterConcrete(IBCGetMsg{},"cosmos-sdk/IBCGetMsg/2",nil)
 }
