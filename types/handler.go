c09624295c1d2dba26d6f18d5fc88a4918a4b54e
Raymond <chengsilei@gmail.com>
2018-08-23 14:11:46 +0800

    keep the common types in sdk

--- types/handler.go
+++ types/handler.go
@@ -5,9 +5,9 @@ import (
 )
 
 // Handler defines the core of the state transition function of an application.
-type Handler func(ctx sdk.Context, msg Msg) Result
+type Handler func(ctx sdk.Context, msg sdk.Msg) sdk.Result
 
 // AnteHandler authenticates transactions, before their internal messages are handled.
 // If newCtx.IsZero(), ctx is used instead.
-type AnteHandler func(ctx sdk.Context, tx Tx) (newCtx sdk.Context, result Result, abort bool)
-type FeeRefundHandler func(ctx sdk.Context, tx Tx, result Result) (Result, error)
+type AnteHandler func(ctx sdk.Context, tx sdk.Tx) (newCtx sdk.Context, result sdk.Result, abort bool)
+type FeeRefundHandler func(ctx sdk.Context, tx sdk.Tx, result sdk.Result) (sdk.Result, error)
