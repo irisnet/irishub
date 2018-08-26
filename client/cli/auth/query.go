ce214715a4981f7a62ad8b266ea2d03a6e34e9bb
zhangzhiqiang <745124335@aa.com>
2018-08-26 10:58:37 +0800

    add coin_type spec and spec

---
+++ client/cli/auth/query.go
@@ -0,0 +1,78 @@
+package auth
+
+import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/wire"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/irisnet/irishub/app"
+	"github.com/spf13/cobra"
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/irisnet/irishub/types"
+)
+
+// GetAccountCmd returns a query account that will display the state of the
+// account at a given address.
+func GetAccountCmd(storeName string, cdc *wire.Codec, decoder auth.AccountDecoder) *cobra.Command {
+	return &cobra.Command{
+		Use:   "account [address]",
+		Short: "Query account balance",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// find the key to look up the account
+			addr := args[0]
+
+			key, err := sdk.AccAddressFromBech32(addr)
+			if err != nil {
+				return err
+			}
+
+			ctx := app.NewContext()
+			cliCtx := ctx.WithCodec(cdc).
+				WithAccountDecoder(decoder)
+			if err := cliCtx.EnsureAccountExistsFromAddr(key); err != nil {
+				return err
+			}
+
+			acc, err := cliCtx.GetAccount(key)
+			if err != nil {
+				return err
+			}
+
+			coins := acc.GetCoins()
+			var coins_str []string
+			for _, coin := range coins {
+				coinName ,_ := types.GetCoinName(coin.String())
+				if ct, err := ctx.GetCoinType(coinName); err == nil {
+					mainCoin, err := ct.Convert(coin.String(), ct.Name)
+					if err == nil {
+						coins_str = append(coins_str, mainCoin)
+					}
+				}
+			}
+			acct := account{
+				Address:       acc.GetAddress(),
+				Coins:         coins_str,
+				PubKey:        acc.GetPubKey(),
+				AccountNumber: acc.GetAccountNumber(),
+				Sequence:      acc.GetSequence(),
+			}
+
+			output, err := wire.MarshalJSONIndent(cdc, acct)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(output))
+			return nil
+		},
+	}
+}
+
+type account struct {
+	Address       sdk.AccAddress `json:"address"`
+	Coins         []string       `json:"coins"`
+	PubKey        crypto.PubKey  `json:"public_key"`
+	AccountNumber int64          `json:"account_number"`
+	Sequence      int64          `json:"sequence"`
+}
