29e37cb49805541c2364a8bdad8af99cec6c709b
xujiacheng <xujiacheng28@outlook.com>
2018-09-14 10:08:03 +0800

    IRISHUB-317:make the errors readable

--- modules/gov/params/gov_params.go
+++ modules/gov/params/gov_params.go
@@ -66,19 +66,19 @@ func (param *DepositProcedureParam) Valid(jsonStr string) sdk.Error {
 	if err = json.Unmarshal([]byte(jsonStr), &param.Value); err == nil {
 
 		if param.Value.MinDeposit[0].Denom != "iris" {
-			return sdk.NewError(gov.DefaultCodespace, 102, fmt.Sprintf("It should be iris "))
+			return sdk.NewError(gov.DefaultCodespace, gov.CodeInvalidMinDepositDenom, fmt.Sprintf("It should be iris "))
 		}
 
 		if param.Value.MinDeposit[0].Amount.GT(sdk.NewInt(10)) && param.Value.MinDeposit[0].Amount.LT(sdk.NewInt(20000)) {
-			return sdk.NewError(gov.DefaultCodespace, 102, fmt.Sprintf("MinDepositAmount should be larger than 10 and less than 20000"))
+			return sdk.NewError(gov.DefaultCodespace, gov.CodeInvalidMinDepositAmount, fmt.Sprintf("MinDepositAmount should be larger than 10 and less than 20000"))
 		}
 
 		if param.Value.MaxDepositPeriod > 20 && param.Value.MaxDepositPeriod < 20000 {
-			return sdk.NewError(gov.DefaultCodespace, 102, fmt.Sprintf("MaxDepositPeriod should be larger than 20 and less than 20000"))
+			return sdk.NewError(gov.DefaultCodespace, gov.CodeInvalidDepositPeriod, fmt.Sprintf("MaxDepositPeriod should be larger than 20 and less than 20000"))
 		}
 
 		return nil
 
 	}
-	return sdk.NewError(gov.DefaultCodespace, 101, fmt.Sprintf("Json is not valid"))
+	return sdk.NewError(gov.DefaultCodespace, gov.CodeInvalidMinDeposit, fmt.Sprintf("Json is not valid"))
 }
