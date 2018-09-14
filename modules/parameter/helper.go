5333212a3b19148eee59e1d860cea8b266397090
xujiacheng <xujiacheng28@outlook.com>
2018-09-14 10:39:43 +0800

    IRISHUB-398:add the genesisData to InitGenesisParameter

--- modules/parameter/helper.go
+++ modules/parameter/helper.go
@@ -4,11 +4,11 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func InitGenesisParameter(p Parameter, ctx sdk.Context) {
+func InitGenesisParameter(p Parameter, ctx sdk.Context, genesisData interface{}) {
 
 	find := p.LoadValue(ctx)
 	if !find {
-		p.InitGenesis(nil)
+		p.InitGenesis(genesisData)
 		p.SaveValue(ctx)
 	}
 
--- modules/parameter/helper_test.go
+++ modules/parameter/helper_test.go
@@ -23,7 +23,8 @@ func defaultContext(key sdk.StoreKey) sdk.Context {
 	return ctx
 }
 
-func TestDepositProcedureParam(t *testing.T) {
+func TestInitGenesisParameter(t *testing.T) {
+
 	skey := sdk.NewKVStoreKey("params")
 	ctx := defaultContext(skey)
 	paramKeeper := params.NewKeeper(wire.NewCodec(), skey)
@@ -32,12 +33,19 @@ func TestDepositProcedureParam(t *testing.T) {
 		MinDeposit:       sdk.Coins{sdk.NewInt64Coin("iris", 10)},
 		MaxDepositPeriod: 1440}
 
+	p2 := gov.DepositProcedure{
+		MinDeposit:       sdk.Coins{sdk.NewInt64Coin("iris", 10)},
+		MaxDepositPeriod: 1440}
+
 	govParam.DepositProcedureParameter.SetReadWriter(paramKeeper.Setter())
 
-	InitGenesisParameter(&govParam.DepositProcedureParameter,ctx)
+	InitGenesisParameter(&govParam.DepositProcedureParameter, ctx,nil)
 
 	require.Equal(t, p1, govParam.DepositProcedureParameter.Value)
 
 	require.Equal(t, govParam.DepositProcedureParameter.ToJson(), "{\"min_deposit\":[{\"denom\":\"iris\",\"amount\":\"10\"}],\"max_deposit_period\":1440}")
 
+	InitGenesisParameter(&govParam.DepositProcedureParameter, ctx, p2)
+
+	require.Equal(t, p1, govParam.DepositProcedureParameter.Value)
 }
