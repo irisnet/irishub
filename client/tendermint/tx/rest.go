97d3e9a0b52a860bd04555c52b7256b58b9fff79
HaoyangLiu <lhy.0318@163.com>
2018-09-18 23:30:24 +0800

    IRISHUB-359: Remove useless broadcast and add tx/send for iris rainbow

--- client/tendermint/tx/rest.go
+++ client/tendermint/tx/rest.go
@@ -10,5 +10,5 @@ import (
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *wire.Codec) {
 	r.HandleFunc("/txs/{hash}", QueryTxRequestHandlerFn(cdc, cliCtx)).Methods("GET")
 	r.HandleFunc("/txs", SearchTxRequestHandlerFn(cliCtx, cdc)).Methods("GET")
-	r.HandleFunc("/txs/broadcast", BroadcastTxRequestHandlerFn(cliCtx, cdc)).Methods("POST")
+	r.HandleFunc("/txs/send", SendTxRequestHandlerFn(cliCtx, cdc)).Methods("POST")
 }
