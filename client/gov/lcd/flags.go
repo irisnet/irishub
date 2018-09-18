9580e5ddc876989676ca4813ab1ffc814c32913b
HaoyangLiu <lhy.0318@163.com>
2018-09-18 22:40:08 +0800

    IRISHUB-359: split PR, remove keys, gov, slashing, stake from lcd

--- client/gov/lcd/flags.go
+++
@@ -1,9 +0,0 @@
-package lcd
-
-const (
-	RestProposalID     = "proposalID"
-	RestDepositer      = "depositer"
-	RestVoter          = "voter"
-	RestProposalStatus = "status"
-	storeName          = "gov"
-)
