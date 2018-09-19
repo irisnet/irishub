54d4ea923f495bb2c7e4982c6cdf9ee1df34d7fb
HaoyangLiu <lhy.0318@163.com>
2018-09-19 10:42:20 +0800

    IRISHUB-360: rename validator address to validator publich key

--- client/slashing/lcd/rest.go
+++ client/slashing/lcd/rest.go
@@ -9,7 +9,7 @@ import (
 
 // RegisterRoutes registers staking-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *wire.Codec, kb keys.Keybase) {
-	r.HandleFunc("/slashing/signing_info/{validator}",
+	r.HandleFunc("/slashing/signing_info/{validator_pub}",
 		signingInfoHandlerFn(cliCtx, "slashing", cdc)).Methods("GET")
 	r.HandleFunc("/slashing/unrevoke",
 		unrevokeRequestHandlerFn(cdc, kb, cliCtx)).Methods("POST")
