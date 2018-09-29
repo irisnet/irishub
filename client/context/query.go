406b18a60c0514f7fa795574973bf98ff5f3677e
HaoyangLiu <lhy.0318@163.com>
2018-09-29 10:16:16 +0800

    IRISHUB-480: change store name and improve error message

--- client/context/query.go
+++ client/context/query.go
@@ -384,13 +384,13 @@ func (cliCtx CLIContext) GetCoinType(coinName string) (types.CoinType, error) {
 		coinType = app.IrisCt
 	} else {
 		key := types.CoinTypeKey(coinName)
-		bz, err := cliCtx.QueryStore([]byte(key), "iparams")
+		bz, err := cliCtx.QueryStore([]byte(key), "params")
 		if err != nil {
 			return coinType, err
 		}
 
 		if bz == nil {
-			return types.CoinType{}, fmt.Errorf("can't find any information about coin type: %s", coinName)
+			return types.CoinType{}, fmt.Errorf("unsupported coin type: %s", coinName)
 		}
 
 		if err = cliCtx.Codec.UnmarshalBinary(bz, &coinType); err != nil {
