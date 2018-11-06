6a49355f56f73b88d21800bcb39654b0cf401250
jiacheng <jiacheng@bianjie.ai>
2018-11-06 10:48:19 +0800

    update the stake rest api

--- client/stake/lcd/sendtx.go
+++ client/stake/lcd/sendtx.go
@@ -2,211 +2,262 @@ package lcd
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/stake"
-	"github.com/cosmos/cosmos-sdk/x/stake/types"
 	"github.com/gorilla/mux"
 	"github.com/irisnet/irishub/client/context"
-	stakeClient "github.com/irisnet/irishub/client/stake"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 	"github.com/irisnet/irishub/client/utils"
 	"net/http"
+	"io/ioutil"
+	"github.com/cosmos/cosmos-sdk/crypto/keys"
+	"github.com/irisnet/irishub/client"
 )
 
-type msgDelegationsInput struct {
-	ValidatorAddr string `json:"validator_addr"` // in bech32
-	Delegation    string `json:"delegation"`
-}
-type msgBeginRedelegateInput struct {
-	ValidatorSrcAddr string `json:"validator_src_addr"` // in bech32
-	ValidatorDstAddr string `json:"validator_dst_addr"` // in bech32
-	SharesAmount     string `json:"shares"`
-}
-type msgCompleteRedelegateInput struct {
-	ValidatorSrcAddr string `json:"validator_src_addr"` // in bech32
-	ValidatorDstAddr string `json:"validator_dst_addr"` // in bech32
-}
-type msgBeginUnbondingInput struct {
-	ValidatorAddr string `json:"validator_addr"` // in bech32
-	SharesAmount  string `json:"shares"`
-}
-type msgCompleteUnbondingInput struct {
-	ValidatorAddr string `json:"validator_addr"` // in bech32
+func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec, kb keys.Keybase) {
+	r.HandleFunc(
+		"/stake/delegators/{delegatorAddr}/delegations",
+		delegationsRequestHandlerFn(cdc, kb, cliCtx),
+	).Methods("POST")
 }
 
-// the request body for edit delegations
-type EditDelegationsBody struct {
-	BaseTx              context.BaseTx               `json:"base_tx"`
-	Delegations         []msgDelegationsInput        `json:"delegations"`
-	BeginUnbondings     []msgBeginUnbondingInput     `json:"begin_unbondings"`
-	CompleteUnbondings  []msgCompleteUnbondingInput  `json:"complete_unbondings"`
-	BeginRedelegates    []msgBeginRedelegateInput    `json:"begin_redelegates"`
-	CompleteRedelegates []msgCompleteRedelegateInput `json:"complete_redelegates"`
-}
+type (
+	msgDelegationsInput struct {
+		DelegatorAddr string   `json:"delegator_addr"` // in bech32
+		ValidatorAddr string   `json:"validator_addr"` // in bech32
+		Delegation    sdk.Coin `json:"delegation"`
+	}
 
-// nolint: gocyclo
+	msgBeginRedelegateInput struct {
+		DelegatorAddr    string `json:"delegator_addr"`     // in bech32
+		ValidatorSrcAddr string `json:"validator_src_addr"` // in bech32
+		ValidatorDstAddr string `json:"validator_dst_addr"` // in bech32
+		SharesAmount     string `json:"shares"`
+	}
+
+	msgBeginUnbondingInput struct {
+		DelegatorAddr string `json:"delegator_addr"` // in bech32
+		ValidatorAddr string `json:"validator_addr"` // in bech32
+		SharesAmount  string `json:"shares"`
+	}
+
+	// the request body for edit delegations
+	EditDelegationsReq struct {
+		BaseReq          context.BaseTx             `json:"base_req"`
+		Delegations      []msgDelegationsInput     `json:"delegations"`
+		BeginUnbondings  []msgBeginUnbondingInput  `json:"begin_unbondings"`
+		BeginRedelegates []msgBeginRedelegateInput `json:"begin_redelegates"`
+	}
+)
+
 // TODO: Split this up into several smaller functions, and remove the above nolint
 // TODO: use sdk.ValAddress instead of sdk.AccAddress for validators in messages
-func delegationsRequestHandlerFn(cdc *codec.Codec, cliCtx context.CLIContext) http.HandlerFunc {
+// TODO: Seriously consider how to refactor...do we need to make it multiple txs?
+// If not, we can just use CompleteAndBroadcastTxREST.
+func delegationsRequestHandlerFn(cdc *codec.Codec, kb keys.Keybase, cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		delegatorAddr := vars["delegatorAddr"]
+		var req EditDelegationsReq
 
-		var m EditDelegationsBody
-		err := utils.ReadPostBody(w, r, cdc, &m)
-		if err != nil {
-			return
-		}
-		cliCtx = utils.InitRequestClictx(cliCtx, r, m.BaseTx.LocalAccountName, delegatorAddr)
-		txCtx, err := context.NewTxContextFromBaseTx(cliCtx, cdc, m.BaseTx)
+		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 
-		delegatorAccAddress, err := sdk.AccAddressFromBech32(delegatorAddr)
+		err = cdc.UnmarshalJSON(body, &req)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(fmt.Sprintf("Couldn't decode delegator. Error: %s", err.Error())))
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
-		if !cliCtx.GenerateOnly {
-			fromAddress, err := cliCtx.GetFromAddress()
-			if err != nil {
-				utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-				return
-			}
-			if !bytes.Equal(fromAddress, delegatorAccAddress) {
-				utils.WriteErrorResponse(w, http.StatusUnauthorized, "Must use own delegator address")
-				return
-			}
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
 		}
+
+		info, err := kb.Get(baseReq.Name)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusUnauthorized, err.Error())
+			return
+		}
+
 		// build messages
-		messages := make([]sdk.Msg, len(m.Delegations)+
-			len(m.BeginRedelegates)+
-			len(m.CompleteRedelegates)+
-			len(m.BeginUnbondings)+
-			len(m.CompleteUnbondings))
+		messages := make([]sdk.Msg, len(req.Delegations)+
+			len(req.BeginRedelegates)+
+			len(req.BeginUnbondings))
 
 		i := 0
-		for _, msg := range m.Delegations {
-
-			validatorAddr, err := sdk.AccAddressFromBech32(msg.ValidatorAddr)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode validator. Error: %s", err.Error())))
-				return
-			}
-
-			delegationToken, err := cliCtx.ParseCoin(msg.Delegation)
+		for _, msg := range req.Delegations {
+			delAddr, err := sdk.AccAddressFromBech32(msg.DelegatorAddr)
 			if err != nil {
 				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 				return
 			}
 
+			valAddr, err := sdk.ValAddressFromBech32(msg.ValidatorAddr)
+			if err != nil {
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+
+			if !bytes.Equal(info.GetPubKey().Address(), delAddr) {
+				utils.WriteErrorResponse(w, http.StatusUnauthorized, "Must use own delegator address")
+				return
+			}
+
 			messages[i] = stake.MsgDelegate{
-				DelegatorAddr: delegatorAccAddress,
-				ValidatorAddr: validatorAddr,
-				Delegation:    delegationToken,
+				DelegatorAddr: delAddr,
+				ValidatorAddr: valAddr,
+				Delegation:    msg.Delegation,
 			}
 
 			i++
 		}
 
-		for _, msg := range m.BeginRedelegates {
-			validatorSrcAddr, err := sdk.AccAddressFromBech32(msg.ValidatorSrcAddr)
+		for _, msg := range req.BeginRedelegates {
+			delAddr, err := sdk.AccAddressFromBech32(msg.DelegatorAddr)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode validator. Error: %s", err.Error())))
-				return
-			}
-			validatorDstAddr, err := sdk.AccAddressFromBech32(msg.ValidatorDstAddr)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode validator. Error: %s", err.Error())))
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 				return
 			}
 
-			shares, err := sdk.NewRatFromDecimal(msg.SharesAmount, types.MaxBondDenominatorPrecision)
+			if !bytes.Equal(info.GetPubKey().Address(), delAddr) {
+				utils.WriteErrorResponse(w, http.StatusUnauthorized, "Must use own delegator address")
+				return
+			}
+
+			valSrcAddr, err := sdk.ValAddressFromBech32(msg.ValidatorSrcAddr)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode shares amount. Error: %s", err.Error())))
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+			valDstAddr, err := sdk.ValAddressFromBech32(msg.ValidatorDstAddr)
+			if err != nil {
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+
+			shares, err := sdk.NewDecFromStr(msg.SharesAmount)
+			if err != nil {
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 				return
 			}
 
 			messages[i] = stake.MsgBeginRedelegate{
-				DelegatorAddr:    delegatorAccAddress,
-				ValidatorSrcAddr: validatorSrcAddr,
-				ValidatorDstAddr: validatorDstAddr,
-				SharesAmount:     shares.Quo(stakeClient.ExRateFromStakeTokenToMainUnit(cliCtx)),
+				DelegatorAddr:    delAddr,
+				ValidatorSrcAddr: valSrcAddr,
+				ValidatorDstAddr: valDstAddr,
+				SharesAmount:     shares,
 			}
 
 			i++
 		}
 
-		for _, msg := range m.CompleteRedelegates {
-			validatorSrcAddr, err := sdk.AccAddressFromBech32(msg.ValidatorSrcAddr)
+		for _, msg := range req.BeginUnbondings {
+			delAddr, err := sdk.AccAddressFromBech32(msg.DelegatorAddr)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode validator. Error: %s", err.Error())))
-				return
-			}
-			validatorDstAddr, err := sdk.AccAddressFromBech32(msg.ValidatorDstAddr)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode validator. Error: %s", err.Error())))
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 				return
 			}
 
-			messages[i] = stake.MsgCompleteRedelegate{
-				DelegatorAddr:    delegatorAccAddress,
-				ValidatorSrcAddr: validatorSrcAddr,
-				ValidatorDstAddr: validatorDstAddr,
-			}
-
-			i++
-		}
-
-		for _, msg := range m.BeginUnbondings {
-			validatorAddr, err := sdk.AccAddressFromBech32(msg.ValidatorAddr)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode validator. Error: %s", err.Error())))
+			if !bytes.Equal(info.GetPubKey().Address(), delAddr) {
+				utils.WriteErrorResponse(w, http.StatusUnauthorized, "Must use own delegator address")
 				return
 			}
 
-			shares, err := sdk.NewRatFromDecimal(msg.SharesAmount, types.MaxBondDenominatorPrecision)
+			valAddr, err := sdk.ValAddressFromBech32(msg.ValidatorAddr)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode shares amount. Error: %s", err.Error())))
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+
+			shares, err := sdk.NewDecFromStr(msg.SharesAmount)
+			if err != nil {
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 				return
 			}
 
 			messages[i] = stake.MsgBeginUnbonding{
-				DelegatorAddr: delegatorAccAddress,
-				ValidatorAddr: validatorAddr,
-				SharesAmount:  shares.Quo(stakeClient.ExRateFromStakeTokenToMainUnit(cliCtx)),
+				DelegatorAddr: delAddr,
+				ValidatorAddr: valAddr,
+				SharesAmount:  shares,
 			}
 
 			i++
 		}
 
-		for _, msg := range m.CompleteUnbondings {
-			validatorAddr, err := sdk.AccAddressFromBech32(msg.ValidatorAddr)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(fmt.Sprintf("Couldn't decode validator. Error: %s", err.Error())))
+		simulateGas, gas, err := client.ReadGasFlag(baseReq.Gas)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		adjustment, ok := utils.ParseFloat64OrReturnBadRequest(w, baseReq.GasAdjustment, client.DefaultGasAdjustment)
+		if !ok {
+			return
+		}
+
+		txBldr := context.TxContext{
+			Codec:         cdc,
+			Gas:           gas,
+			GasAdjustment: adjustment,
+			SimulateGas:   simulateGas,
+			ChainID:       baseReq.ChainID,
+		}
+
+		// sign messages
+		signedTxs := make([][]byte, len(messages[:]))
+		for i, msg := range messages {
+			// increment sequence for each message
+			txBldr = txBldr.WithAccountNumber(baseReq.AccountNumber)
+			txBldr = txBldr.WithSequence(baseReq.Sequence)
+
+			baseReq.Sequence++
+
+			if utils.HasDryRunArg(r) || txBldr.SimulateGas {
+				newBldr, err := utils.EnrichCtxWithGas(txBldr, cliCtx, baseReq.Name, []sdk.Msg{msg})
+				if err != nil {
+					utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+					return
+				}
+
+				if utils.HasDryRunArg(r) {
+					utils.WriteSimulationResponse(w, newBldr.Gas)
+					return
+				}
+
+				txBldr = newBldr
+			}
+
+			if utils.HasGenerateOnlyArg(r) {
+				utils.WriteGenerateStdTxResponse(w, txBldr, []sdk.Msg{msg})
 				return
 			}
 
-			messages[i] = stake.MsgCompleteUnbonding{
-				DelegatorAddr: delegatorAccAddress,
-				ValidatorAddr: validatorAddr,
+			txBytes, err := txBldr.BuildAndSign(baseReq.Name, baseReq.Password, []sdk.Msg{msg})
+			if err != nil {
+				utils.WriteErrorResponse(w, http.StatusUnauthorized, err.Error())
+				return
 			}
-			i++
+
+			signedTxs[i] = txBytes
 		}
 
-		utils.SendOrReturnUnsignedTx(w, cliCtx, txCtx, m.BaseTx, messages)
+		// send
+		// XXX the operation might not be atomic if a tx fails
+		//     should we have a sdk.MultiMsg type to make sending atomic?
+		results := make([]*ctypes.ResultBroadcastTxCommit, len(signedTxs[:]))
+		for i, txBytes := range signedTxs {
+			res, err := cliCtx.BroadcastTx(txBytes)
+			if err != nil {
+				utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+
+			results[i] = res
+		}
+
+		utils.PostProcessResponse(w, cdc, results, cliCtx.Indent)
 	}
 }
