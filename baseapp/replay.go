80946d171b4e3a6848c271d7c363659a0c96080b
jiacheng <jiacheng@bianjie.ai>
2018-11-14 11:15:48 +0800

    move irisapp.replay to Replay

---
+++ baseapp/replay.go
@@ -0,0 +1,54 @@
+package baseapp
+
+import (
+	"github.com/spf13/viper"
+	"fmt"
+	bc "github.com/tendermint/tendermint/blockchain"
+	tmcli "github.com/tendermint/tendermint/libs/cli"
+	dbm "github.com/tendermint/tendermint/libs/db"
+	"github.com/tendermint/tendermint/libs/log"
+	"github.com/tendermint/tendermint/node"
+	sm "github.com/tendermint/tendermint/state"
+
+	"github.com/cosmos/cosmos-sdk/server"
+)
+
+func Replay(logger log.Logger) int64 {
+	ctx := server.NewDefaultContext()
+	ctx.Config.RootDir = viper.GetString(tmcli.HomeFlag)
+	dbContext := node.DBContext{"state", ctx.Config}
+	dbType := dbm.DBBackendType(dbContext.Config.DBBackend)
+	stateDB := dbm.NewDB(dbContext.ID, dbType, dbContext.Config.DBDir())
+
+	blockDBContext := node.DBContext{"blockstore", ctx.Config}
+	blockStoreDB := dbm.NewDB(blockDBContext.ID, dbType, dbContext.Config.DBDir())
+	blockStore := bc.NewBlockStore(blockStoreDB)
+
+	defer func() {
+		stateDB.Close()
+		blockStoreDB.Close()
+	}()
+
+	curState := sm.LoadState(stateDB)
+	preState := sm.LoadPreState(stateDB)
+	if curState.LastBlockHeight == preState.LastBlockHeight {
+		panic(fmt.Errorf("there is no block now, can't replay"))
+	}
+	var loadHeight int64
+	if blockStore.Height() == curState.LastBlockHeight {
+		logger.Info(fmt.Sprintf("blockstore height equals to current state height %d", curState.LastBlockHeight))
+		logger.Info("Just reset state DB to last height")
+		sm.SaveState(stateDB, preState)
+		loadHeight = preState.LastBlockHeight
+	} else if blockStore.Height() == curState.LastBlockHeight+1 {
+		logger.Info(fmt.Sprintf("blockstore height %d, current state height %d", blockStore.Height(), curState.LastBlockHeight))
+		logger.Info(fmt.Sprintf("Retreat block %d in block store and reset state DB to last height", blockStore.Height()))
+		blockStore.RetreatLastBlock()
+		sm.SaveState(stateDB, preState)
+		loadHeight = preState.LastBlockHeight
+	} else {
+		panic(fmt.Errorf("tendermint block store height should be at most one ahead of the its state height"))
+	}
+
+	return loadHeight
+}
--- examples/irishub1/app/app.go
+++ examples/irishub1/app/app.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -10,7 +9,6 @@ import (
 	"strings"
 
 	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/server"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/bank"
@@ -32,13 +30,9 @@ import (
 	"github.com/irisnet/irishub/modules/upgrade/params"
 	"github.com/spf13/viper"
 	abci "github.com/tendermint/tendermint/abci/types"
-	bc "github.com/tendermint/tendermint/blockchain"
-	tmcli "github.com/tendermint/tendermint/libs/cli"
 	cmn "github.com/tendermint/tendermint/libs/common"
 	dbm "github.com/tendermint/tendermint/libs/db"
 	"github.com/tendermint/tendermint/libs/log"
-	"github.com/tendermint/tendermint/node"
-	sm "github.com/tendermint/tendermint/state"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
 
@@ -127,7 +121,7 @@ func NewIrisApp(logger log.Logger, db dbm.DB, traceStore io.Writer, baseAppOptio
 
 	var lastHeight int64
 	if viper.GetBool(FlagReplay) {
-		lastHeight = app.replay()
+		lastHeight = bam.Replay(app.Logger)
 	}
 
 	// define the AccountKeeper
@@ -505,40 +499,6 @@ func (app *IrisApp) runMsgs(ctx sdk.Context, msgs []sdk.Msg, mode bam.RunTxMode)
 	}
 
 	return result
-}
-
-func (app *IrisApp) replay() int64 {
-	ctx := server.NewDefaultContext()
-	ctx.Config.RootDir = viper.GetString(tmcli.HomeFlag)
-	dbContext := node.DBContext{"state", ctx.Config}
-	dbType := dbm.DBBackendType(dbContext.Config.DBBackend)
-	stateDB := dbm.NewDB(dbContext.ID, dbType, dbContext.Config.DBDir())
-
-	blockDBContext := node.DBContext{"blockstore", ctx.Config}
-	blockStoreDB := dbm.NewDB(blockDBContext.ID, dbType, dbContext.Config.DBDir())
-	blockStore := bc.NewBlockStore(blockStoreDB)
-
-	defer func() {
-		stateDB.Close()
-		blockStoreDB.Close()
-	}()
-
-	curState := sm.LoadState(stateDB)
-	preState := sm.LoadPreState(stateDB)
-	if curState.LastBlockHeight == preState.LastBlockHeight {
-		panic(errors.New("there is no block now, can't replay"))
-	}
-	var loadHeight int64
-	if blockStore.Height() == curState.LastBlockHeight {
-		sm.SaveState(stateDB, preState)
-		loadHeight = preState.LastBlockHeight
-	} else if blockStore.Height() == curState.LastBlockHeight+1 {
-		loadHeight = curState.LastBlockHeight
-	} else {
-		panic(errors.New("tendermint block store height should be at most one ahead of the its state height"))
-	}
-
-	return loadHeight
 }
 
 //______________________________________________________________________________________________
