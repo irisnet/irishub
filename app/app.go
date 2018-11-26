9aaa3b4ab9954acae2e9ff15998d6be86f740494
HaoyangLiu <lhy.0318@163.com>
2018-11-26 13:42:02 +0800

    Change dependency

--- app/app.go
+++ app/app.go
@@ -32,7 +32,6 @@ import (
 	dbm "github.com/tendermint/tendermint/libs/db"
 	"github.com/tendermint/tendermint/libs/log"
 	tmtypes "github.com/tendermint/tendermint/types"
-	sm "github.com/tendermint/tendermint/state"
 )
 
 const (
@@ -306,9 +305,6 @@ func (app *IrisApp) EndBlocker(ctx sdk.Context, req abci.RequestEndBlock) abci.R
 	tags := gov.EndBlocker(ctx, app.govKeeper)
 	validatorUpdates := stake.EndBlocker(ctx, app.stakeKeeper)
 	tags = tags.AppendTags(upgrade.EndBlocker(ctx, app.upgradeKeeper))
-	if ctx.BlockHeight() == 5 {
-		tags = tags.AppendTag(sm.TerminateTagKey, []byte(sm.TerminateTagValue))
-	}
 	return abci.ResponseEndBlock{
 		ValidatorUpdates: validatorUpdates,
 		Tags:             tags,
