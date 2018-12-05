43a376c22cd4f6611fc6c2000ab65534c001857c
Raymond <chengsilei@gmail.com>
2018-12-05 16:37:12 +0800

    refactor bank module

---
+++ handlers/bank/handler.go
@@ -0,0 +1,41 @@
+package bank
+
+import (
+	sdk "github.com/irisnet/irishub/types"
+	"github.com/irisnet/irishub/types/bank"
+)
+
+// NewHandler returns a handler for "bank" type messages.
+func NewHandler(k Keeper) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
+		switch msg := msg.(type) {
+		case bank.MsgSend:
+			return handleMsgSend(ctx, k, msg)
+		case bank.MsgIssue:
+			return handleMsgIssue(ctx, k, msg)
+		default:
+			errMsg := "Unrecognized bank Msg type: %s" + msg.Type()
+			return sdk.ErrUnknownRequest(errMsg).Result()
+		}
+	}
+}
+
+// Handle MsgSend.
+func handleMsgSend(ctx sdk.Context, k Keeper, msg bank.MsgSend) sdk.Result {
+	// NOTE: totalIn == totalOut should already have been checked
+
+	tags, err := k.InputOutputCoins(ctx, msg.Inputs, msg.Outputs)
+	if err != nil {
+		return err.Result()
+	}
+
+	return sdk.Result{
+		Tags: tags,
+	}
+}
+
+// Handle MsgIssue.
+func handleMsgIssue(ctx sdk.Context, k Keeper, msg bank.MsgIssue) sdk.Result {
+	panic("not implemented yet")
+}
+
--- types/bank/codec.go
+++ types/bank/codec.go
@@ -10,6 +10,8 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterInterface((*Account)(nil), nil)
 	cdc.RegisterConcrete(&BaseAccount{}, "auth/Account", nil)
 	cdc.RegisterConcrete(StdTx{}, "auth/StdTx", nil)
+	cdc.RegisterConcrete(MsgSend{}, "cosmos-sdk/Send", nil)
+	cdc.RegisterConcrete(MsgIssue{}, "cosmos-sdk/Issue", nil)
 }
 
 var msgCdc = codec.New()
