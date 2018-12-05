c78fec575fee83c2281b18d216da06649fd8c709
Raymond <chengsilei@gmail.com>
2018-12-05 16:16:55 +0800

    R4R: refactor the auth module (#768)
    * refactor the auth module
    
    * move fee into /keepers/fee

---
+++ keepers/fee/fee.go
@@ -0,0 +1,72 @@
+package fee
+
+import (
+	codec "github.com/irisnet/irishub/codec"
+	sdk "github.com/irisnet/irishub/types"
+)
+
+var (
+	collectedFeesKey = []byte("collectedFees")
+)
+
+// This FeeCollectionKeeper handles collection of fees in the anteHandler
+// and setting of MinFees for different fee tokens
+type FeeCollectionKeeper struct {
+
+	// The (unexposed) key used to access the fee store from the Context.
+	key sdk.StoreKey
+
+	// The codec codec for binary encoding/decoding of accounts.
+	cdc *codec.Codec
+}
+
+func NewFeeCollectionKeeper(cdc *codec.Codec, key sdk.StoreKey) FeeCollectionKeeper {
+	return FeeCollectionKeeper{
+		key: key,
+		cdc: cdc,
+	}
+}
+
+// retrieves the collected fee pool
+func (fck FeeCollectionKeeper) GetCollectedFees(ctx sdk.Context) sdk.Coins {
+	store := ctx.KVStore(fck.key)
+	bz := store.Get(collectedFeesKey)
+	if bz == nil {
+		return sdk.Coins{}
+	}
+
+	feePool := &(sdk.Coins{})
+	fck.cdc.MustUnmarshalBinaryLengthPrefixed(bz, feePool)
+	return *feePool
+}
+
+func (fck FeeCollectionKeeper) setCollectedFees(ctx sdk.Context, coins sdk.Coins) {
+	bz := fck.cdc.MustMarshalBinaryLengthPrefixed(coins)
+	store := ctx.KVStore(fck.key)
+	store.Set(collectedFeesKey, bz)
+}
+
+// add to the fee pool
+func (fck FeeCollectionKeeper) AddCollectedFees(ctx sdk.Context, coins sdk.Coins) sdk.Coins {
+	newCoins := fck.GetCollectedFees(ctx).Plus(coins)
+	fck.setCollectedFees(ctx, newCoins)
+
+	return newCoins
+}
+
+////////////////////  iris/cosmos-sdk begin///////////////////////////
+// RefundCollectedFees deducts fees from fee collector
+func (fck FeeCollectionKeeper) RefundCollectedFees(ctx sdk.Context, coins sdk.Coins) sdk.Coins {
+	newCoins := fck.GetCollectedFees(ctx).Minus(coins)
+	if !newCoins.IsNotNegative() {
+		panic("fee collector contains negative coins")
+	}
+	fck.setCollectedFees(ctx, newCoins)
+	return newCoins
+}
+////////////////////  iris/cosmos-sdk end///////////////////////////
+
+// clear the fee pool
+func (fck FeeCollectionKeeper) ClearCollectedFees(ctx sdk.Context) {
+	fck.setCollectedFees(ctx, sdk.Coins{})
+}
