e87468863ed8577c33d1d254e28e6a25ab41f2d1
chengwenxi <vincent.ch.cn@gmail.com>
2018-12-10 17:44:04 +0800

    initialize guardian keeper first

--- app/v0/protocol_v0.go
+++ app/v0/protocol_v0.go
@@ -112,6 +112,11 @@ func (p *ProtocolVersion0) configKeepers() {
 	)
 
 	// add handlers
+	p.guardianKeeper = guardian.NewKeeper(
+		p.cdc,
+		protocol.KeyGuardian,
+		guardian.DefaultCodespace,
+	)
 	p.bankKeeper = bank.NewBaseKeeper(p.accountMapper)
 	p.feeCollectionKeeper = auth.NewFeeCollectionKeeper(
 		p.cdc,
@@ -166,11 +171,6 @@ func (p *ProtocolVersion0) configKeepers() {
 		p.bankKeeper,
 		p.guardianKeeper,
 		service.DefaultCodespace,
-	)
-	p.guardianKeeper = guardian.NewKeeper(
-		p.cdc,
-		protocol.KeyGuardian,
-		guardian.DefaultCodespace,
 	)
 
 	// register the staking hooks
