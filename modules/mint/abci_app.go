33a62ef166c3e640a6db979d99bc9cdbffbe4a6c
HaoyangLiu <lhy.0318@163.com>
2018-12-24 17:02:31 +0800

    Update mint unit test

--- modules/mint/abci_app.go
+++ modules/mint/abci_app.go
@@ -8,6 +8,7 @@ import (
 // Called every block, process inflation on the first block of every hour
 func BeginBlocker(ctx sdk.Context, k Keeper) sdk.Tags {
 
+	// Get block BFT time and block height
 	blockTime := ctx.BlockHeader().Time
 	blockHeight := ctx.BlockHeader().Height
 	minter := k.GetMinter(ctx)
@@ -17,17 +18,21 @@ func BeginBlocker(ctx sdk.Context, k Keeper) sdk.Tags {
 		return nil
 	}
 
+	// Calculate block mint amount
 	params := k.GetParams(ctx)
 	minter.AnnualProvisions = minter.NextAnnualProvisions(params)
 	mintedCoin := minter.BlockProvision(params, blockTime)
 
+	// Increase loosen token and add minted coin to feeCollector
 	k.bk.IncreaseLoosenToken(ctx, sdk.Coins{mintedCoin})
 	k.fck.AddCollectedFees(ctx, sdk.Coins{mintedCoin})
 
+	// Update last block BFT time
 	lastInflationTime := minter.LastUpdate
 	minter.LastUpdate = blockTime
 	k.SetMinter(ctx, minter)
 
+	// Add tags
 	return sdk.NewTags(
 		tags.LastInflationTime, []byte(lastInflationTime.String()),
 		tags.InflationTime, []byte(blockTime.String()),
--- modules/mint/params.go
+++ modules/mint/params.go
@@ -9,16 +9,16 @@ import (
 
 // mint parameters
 type Params struct {
-	MintDenom string  `json:"mint_denom"` // type of coin to mint
-	Inflation sdk.Dec `json:"inflation"`  // inflation rate
+	MintDenom         string  `json:"mint_denom"` // type of coin to mint
+	Inflation         sdk.Dec `json:"inflation"`  // inflation rate
 	InflationBasement sdk.Int `json:"inflation_basement"`
 }
 
 // default minting module parameters
 func DefaultParams() Params {
 	return Params{
-		MintDenom: stakeTypes.StakeDenom,
-		Inflation: sdk.NewDecWithPrec(4, 2),
+		MintDenom:         stakeTypes.StakeDenom,
+		Inflation:         sdk.NewDecWithPrec(4, 2),
 		InflationBasement: sdk.NewIntWithDecimal(2, 9).Mul(sdk.NewIntWithDecimal(1, 18)), // 2*(10^9)iris, 2*(10^9)*(10^18)iris-atto
 	}
 }
