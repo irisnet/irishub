3426836e5194a81cc855c623714d9a0b487f5f03
jiacheng <jiacheng@bianjie.ai>
2018-12-28 11:19:15 +0800

    replace cosmos-sdk with irishub/modulename in codec

--- modules/bank/codec.go
+++ modules/bank/codec.go
@@ -6,8 +6,8 @@ import (
 
 // Register concrete types on codec codec
 func RegisterCodec(cdc *codec.Codec) {
-	cdc.RegisterConcrete(MsgSend{}, "cosmos-sdk/bank/Send", nil)
-	cdc.RegisterConcrete(MsgIssue{}, "cosmos-sdk/bank/Issue", nil)
+	cdc.RegisterConcrete(MsgSend{}, "irishub/bank/Send", nil)
+	cdc.RegisterConcrete(MsgIssue{}, "irishub/bank/Issue", nil)
 }
 
 var msgCdc = codec.New()
