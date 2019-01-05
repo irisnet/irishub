ab38bac62c9c2368ea9eba893d5ec5433fc3fff0
Haifeng Xi <haifeng@bianjie.ai>
2019-01-05 19:44:05 +0800

    define switch heights for protocol patches

---
+++ app/protocol/patches.go
@@ -0,0 +1,11 @@
+package protocol
+
+/*
+	For each patch (a.k.a. class3 upgrade), define the corresponding height
+	at which the software switches to the patched logic
+*/
+const (
+//	001_PATCH_HEIGHT = 15300	// 001: 0 - protocol version, 01 - patch number for protocol v0
+//	101_PATCH_HEIGHT = 26000
+//	102_PATCH_HEIGHT = 75000
+)
