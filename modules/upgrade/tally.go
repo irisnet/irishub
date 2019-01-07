ea4c22dbcfdd0f1d482162f4c9a1b9bef9f47278
Raymond <chengsilei@gmail.com>
2019-01-07 18:03:06 +0800

    only use the bonded validator for upgrade tally and gov slash

--- modules/upgrade/tally.go
+++ modules/upgrade/tally.go
@@ -9,13 +9,16 @@ func tally(ctx sdk.Context,versionProtocol uint64, k Keeper) (passes bool) {
 	totalVotingPower := sdk.ZeroDec()
 	switchVotingPower := sdk.ZeroDec()
 
-	for _, validator := range k.sk.GetAllValidators(ctx) {
+	k.sk.IterateBondedValidatorsByPower(ctx, func(index int64, validator sdk.Validator) (stop bool) {
 		totalVotingPower = totalVotingPower.Add(validator.GetPower())
-		valAcc := validator.ConsAddress().String()
+		valAcc := validator.GetConsAddr().String()
 		if ok := k.GetSignal(ctx, versionProtocol, valAcc); ok {
 			switchVotingPower = switchVotingPower.Add(validator.GetPower())
 		}
-	}
+
+		return false
+	})
+
 	// If more than 95% of validator update , do switch
 	if switchVotingPower.Quo(totalVotingPower).GT(GetUpgradeThreshlod(ctx)) {
 		return true
