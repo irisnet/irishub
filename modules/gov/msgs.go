a595287109e1edc030eb8f1158a38953cda5ad08
chengwenxi <vincent.ch.cn@gmail.com>
2019-01-16 15:24:58 +0800

    fix msg length check

--- modules/gov/msgs.go
+++ modules/gov/msgs.go
@@ -114,6 +114,11 @@ func (msg MsgSubmitSoftwareUpgradeProposal) ValidateBasic() sdk.Error {
 	if err != nil {
 		return err
 	}
+
+	if len(msg.Software) > 70 {
+		return sdk.ErrInvalidLength(DefaultCodespace, CodeInvalidProposal, "software", len(msg.Software), 70)
+	}
+
 	// if threshold not in [0.85,1), then print error
 	if msg.Threshold.LT(sdk.NewDecWithPrec(85, 2)) || msg.Threshold.GTE(sdk.NewDec(1)) {
 		return ErrInvalidUpgradeThreshold(DefaultCodespace, msg.Threshold)
