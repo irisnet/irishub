082db2b6b0f6c070346468266591cb8560c6db29
HaoyangLiu <lhy.0318@163.com>
2019-01-21 16:41:13 +0800

    Handle expected abort during replay last block

--- app/replay.go
+++ app/replay.go
@@ -30,23 +30,28 @@ func Replay(logger log.Logger) int64 {
 
 	curState := sm.LoadState(stateDB)
 	preState := sm.LoadPreState(stateDB)
-	if curState.LastBlockHeight <= preState.LastBlockHeight {
+	if curState.LastBlockHeight == preState.LastBlockHeight && preState.LastBlockHeight == 0 {
 		panic(fmt.Errorf("there is no block now, can't replay"))
 	}
 	var loadHeight int64
 	if blockStore.Height() == curState.LastBlockHeight {
-		logger.Info(fmt.Sprintf("blockstore height equals to current state height %d", curState.LastBlockHeight))
+		logger.Info(fmt.Sprintf("Blockstore height equals to current state height %d", curState.LastBlockHeight))
 		logger.Info("Just reset state DB to last height")
 		sm.SaveState(stateDB, preState)
 		loadHeight = preState.LastBlockHeight
 	} else if blockStore.Height() == curState.LastBlockHeight+1 {
-		logger.Info(fmt.Sprintf("blockstore height %d, current state height %d", blockStore.Height(), curState.LastBlockHeight))
+		logger.Info(fmt.Sprintf("Blockstore height %d, current state height %d", blockStore.Height(), curState.LastBlockHeight))
 		logger.Info(fmt.Sprintf("Retreat block %d in block store and reset state DB to last height", blockStore.Height()))
 		blockStore.RetreatLastBlock()
 		sm.SaveState(stateDB, preState)
 		loadHeight = preState.LastBlockHeight
+	} else if blockStore.Height() == curState.LastBlockHeight+2 && curState.LastBlockHeight == preState.LastBlockHeight {
+		logger.Info(fmt.Sprintf("Blockstore height %d, current state height %d, pre-state height %d", blockStore.Height(), curState.LastBlockHeight, preState.LastBlockHeight))
+		logger.Info("State store has already been retreat to last height, only retreat block store to last height")
+		blockStore.RetreatLastBlock()
+		loadHeight = preState.LastBlockHeight
 	} else {
-		panic(fmt.Errorf("tendermint block store height should be at most one ahead of the its state height"))
+		panic(fmt.Errorf("unexpected situation: tendermint block store height %d, tendermint state store height %d", blockStore.Height(), curState.LastBlockHeight))
 	}
 
 	return loadHeight
