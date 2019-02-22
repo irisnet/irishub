6071a6f768b0352a0e805b4fb39272281640b0da
HaoyangLiu <lhy.0318@163.com>
2019-02-22 13:49:23 +0800

    Add default CoinFlowTags collector in create context

--- types/context.go
+++ types/context.go
@@ -51,7 +51,7 @@ func NewContext(ms MultiStore, header abci.Header, isCheckTx bool, logger log.Lo
 	c = c.WithConsensusParams(nil)
 	c = c.WithCheckValidNum(0)
 	c = c.WithCoinFlowTrigger("")
-	c = c.WithCoinFlowTags(nil)
+	c = c.WithCoinFlowTags(NewCoinFlowRecord(false))
 	return c
 }
 
