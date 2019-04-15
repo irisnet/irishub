b1cf65e927f22e651f2524f45b0f95d249c42e65
chengwenxi <vincent.ch.cn@gmail.com>
2019-04-15 19:14:53 +0800

    Refactor service lcd

--- client/service/lcd/query.go
+++ client/service/lcd/query.go
@@ -9,11 +9,9 @@ import (
 	"github.com/irisnet/irishub/client/context"
 	"github.com/irisnet/irishub/modules/service"
 	"github.com/irisnet/irishub/client/utils"
-	cmn "github.com/irisnet/irishub/client/service"
 	sdk "github.com/irisnet/irishub/types"
+	"github.com/irisnet/irishub/app/protocol"
 )
-
-const storeName = "service"
 
 func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) {
 
@@ -60,38 +58,25 @@ func definitionGetHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec) http.Ha
 		defChainId := vars[DefChainId]
 		serviceName := vars[ServiceName]
 
-		res, err := cliCtx.QueryStore(service.GetServiceDefinitionKey(defChainId, serviceName), storeName)
+		params := service.QueryServiceParams{
+			DefChainID:  defChainId,
+			ServiceName: serviceName,
+		}
+
+		bz, err := cdc.MarshalJSON(params)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		route := fmt.Sprintf("custom/%s/%s", protocol.ServiceRoute, service.QueryDefinition)
+		res, err := cliCtx.QueryWithData(route, bz)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		if len(res) == 0 {
-			utils.WriteErrorResponse(w, http.StatusNoContent, "")
-			return
-		}
 
-		var svcDef service.SvcDef
-		cdc.MustUnmarshalBinaryLengthPrefixed(res, &svcDef)
-
-		res1, err := cliCtx.QuerySubspace(service.GetMethodsSubspaceKey(defChainId, serviceName), storeName)
-		if err != nil {
-			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-
-		var methods []service.MethodProperty
-		for _, re := range res1 {
-			var method service.MethodProperty
-			cdc.MustUnmarshalBinaryLengthPrefixed(re.Value, &method)
-			methods = append(methods, method)
-		}
-
-		output, err := codec.MarshalJSONIndent(cdc, cmn.DefOutput{Definition: svcDef, Methods: methods})
-		if err != nil {
-			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		utils.PostProcessResponse(w, cdc, output, cliCtx.Indent)
+		utils.PostProcessResponse(w, cdc, res, cliCtx.Indent)
 	}
 }
 
@@ -103,28 +88,32 @@ func bindingHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec) http.HandlerF
 		bindChainId := vars[BindChainId]
 		bechProviderAddr := vars[Provider]
 
-		providerAddr, err := sdk.AccAddressFromBech32(bechProviderAddr)
+		provider, err := sdk.AccAddressFromBech32(bechProviderAddr)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
-		res, err := cliCtx.QueryStore(service.GetServiceBindingKey(defChainId, serviceName, bindChainId, providerAddr), storeName)
+
+		params := service.QueryBindingParams{
+			DefChainID:  defChainId,
+			ServiceName: serviceName,
+			BindChainId: bindChainId,
+			Provider:    provider,
+		}
+
+		bz, err := cdc.MarshalJSON(params)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		route := fmt.Sprintf("custom/%s/%s", protocol.ServiceRoute, service.QueryBinding)
+		res, err := cliCtx.QueryWithData(route, bz)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		if len(res) == 0 {
-			utils.WriteErrorResponse(w, http.StatusNoContent, "")
-			return
-		}
-		var svcBinding service.SvcBinding
-		cdc.MustUnmarshalBinaryLengthPrefixed(res, &svcBinding)
-		output, err := codec.MarshalJSONIndent(cdc, svcBinding)
-		if err != nil {
-			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		utils.PostProcessResponse(w, cdc, output, cliCtx.Indent)
+		utils.PostProcessResponse(w, cdc, res, cliCtx.Indent)
 	}
 }
 
@@ -134,28 +123,25 @@ func bindingsHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec) http.Handler
 		defChainId := vars[DefChainId]
 		serviceName := vars[ServiceName]
 
-		res, err := cliCtx.QuerySubspace(service.GetBindingsSubspaceKey(defChainId, serviceName), storeName)
-		if err != nil {
-			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		if len(res) == 0 {
-			utils.WriteErrorResponse(w, http.StatusNoContent, "")
-			return
-		}
-		var bindings []service.SvcBinding
-		for _, re := range res {
-			var binding service.SvcBinding
-			cdc.MustUnmarshalBinaryLengthPrefixed(re.Value, &binding)
-			bindings = append(bindings, binding)
+		params := service.QueryServiceParams{
+			DefChainID:  defChainId,
+			ServiceName: serviceName,
 		}
 
-		output, err := codec.MarshalJSONIndent(cdc, bindings)
+		bz, err := cdc.MarshalJSON(params)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		route := fmt.Sprintf("custom/%s/%s", protocol.ServiceRoute, service.QueryBindings)
+		res, err := cliCtx.QueryWithData(route, bz)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		utils.PostProcessResponse(w, cdc, output, cliCtx.Indent)
+
+		utils.PostProcessResponse(w, cdc, res, cliCtx.Indent)
 	}
 }
 
@@ -167,34 +153,33 @@ func requestsHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec) http.Handler
 		bindChainId := vars[BindChainId]
 		bechProviderAddr := vars[Provider]
 
-		providerAddr, err := sdk.AccAddressFromBech32(bechProviderAddr)
+		provider, err := sdk.AccAddressFromBech32(bechProviderAddr)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 
-		res, err := cliCtx.QuerySubspace(service.GetSubActiveRequestKey(defChainId, serviceName, bindChainId, providerAddr), storeName)
-		if err != nil {
-			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		if len(res) == 0 {
-			utils.WriteErrorResponse(w, http.StatusNoContent, "")
-			return
-		}
-		var reqs []service.SvcRequest
-		for _, re := range res {
-			var req service.SvcRequest
-			cdc.MustUnmarshalBinaryLengthPrefixed(re.Value, &req)
-			reqs = append(reqs, req)
+		params := service.QueryBindingParams{
+			DefChainID:  defChainId,
+			ServiceName: serviceName,
+			BindChainId: bindChainId,
+			Provider:    provider,
 		}
 
-		output, err := codec.MarshalJSONIndent(cdc, reqs)
+		bz, err := cdc.MarshalJSON(params)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		route := fmt.Sprintf("custom/%s/%s", protocol.ServiceRoute, service.QueryRequests)
+		res, err := cliCtx.QueryWithData(route, bz)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		utils.PostProcessResponse(w, cdc, output, cliCtx.Indent)
+
+		utils.PostProcessResponse(w, cdc, res, cliCtx.Indent)
 	}
 }
 
@@ -204,29 +189,25 @@ func responseGetHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec) http.Hand
 		reqChainId := vars[ReqChainId]
 		reqId := vars[ReqId]
 
-		eHeight, rHeight, counter, err := service.ConvertRequestID(reqId)
+		params := service.QueryResponseParams{
+			ReqChainId: reqChainId,
+			RequestId:  reqId,
+		}
+
+		bz, err := cdc.MarshalJSON(params)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 
-		res, err := cliCtx.QueryStore(service.GetResponseKey(reqChainId, eHeight, rHeight, counter), storeName)
+		route := fmt.Sprintf("custom/%s/%s", protocol.ServiceRoute, service.QueryResponse)
+		res, err := cliCtx.QueryWithData(route, bz)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		if len(res) == 0 {
-			utils.WriteErrorResponse(w, http.StatusNoContent, "")
-			return
-		}
-		var resp service.SvcResponse
-		cdc.MustUnmarshalBinaryLengthPrefixed(res, &resp)
-		output, err := codec.MarshalJSONIndent(cdc, resp)
-		if err != nil {
-			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		utils.PostProcessResponse(w, cdc, output, cliCtx.Indent)
+
+		utils.PostProcessResponse(w, cdc, res, cliCtx.Indent)
 	}
 }
 
@@ -241,31 +222,23 @@ func feesHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec) http.HandlerFunc
 			return
 		}
 
-		res, err := cliCtx.QueryStore(service.GetReturnedFeeKey(address), storeName)
-		if err != nil {
-			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		var returnedFee service.ReturnedFee
-		if len(res) > 0 {
-			cdc.MustUnmarshalBinaryLengthPrefixed(res, &returnedFee)
+		params := service.QueryFeesParams{
+			Address: address,
 		}
 
-		res1, err := cliCtx.QueryStore(service.GetIncomingFeeKey(address), storeName)
-		var incomingFee service.IncomingFee
+		bz, err := cdc.MarshalJSON(params)
 		if err != nil {
-			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
-		}
-		if len(res1) > 0 {
-			cdc.MustUnmarshalBinaryLengthPrefixed(res1, &incomingFee)
 		}
 
-		output, err := codec.MarshalJSONIndent(cdc, cmn.FeesOutput{ReturnedFee: returnedFee.Coins, IncomingFee: incomingFee.Coins})
+		route := fmt.Sprintf("custom/%s/%s", protocol.ServiceRoute, service.QueryFees)
+		res, err := cliCtx.QueryWithData(route, bz)
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		utils.PostProcessResponse(w, cdc, output, cliCtx.Indent)
+
+		utils.PostProcessResponse(w, cdc, res, cliCtx.Indent)
 	}
 }
