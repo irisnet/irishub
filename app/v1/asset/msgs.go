48b0df35622f79dad40f52f108b2f00c904b26c7
secret <sufay30281@gmail.com>
2019-05-31 16:06:12 +0800

    implement ValidateBasic for MsgEditGateway

--- app/v1/asset/msgs.go
+++ app/v1/asset/msgs.go
@@ -107,14 +107,14 @@ type MsgEditGateway struct {
 }
 
 // NewMsgEditGateway creates a MsgEditGateway
-func NewMsgEditGateway(identity, moniker, details, website string, redeemAddr, owner sdk.AccAddress, operators []sdk.AccAddress) MsgEditGateway {
+func NewMsgEditGateway(owner sdk.AccAddress, moniker, identity, details, website string, redeemAddr sdk.AccAddress, operators []sdk.AccAddress) MsgEditGateway {
 	return MsgEditGateway{
-		Identity:   identity,
+		Owner:      owner,
 		Moniker:    moniker,
+		Identity:   identity,
 		Details:    details,
 		Website:    website,
 		RedeemAddr: redeemAddr,
-		Owner:      owner,
 		Operators:  operators,
 	}
 }
@@ -127,13 +127,34 @@ func (msg MsgEditGateway) Type() string { return "edit_gateway" }
 
 // ValidateBasic implements Msg
 func (msg MsgEditGateway) ValidateBasic() sdk.Error {
-	// TODO
+	// check the moniker
+	if len(msg.Moniker) == 0 || uint32(len(msg.Moniker)) > MaximumGatewayMonikerSize {
+		return ErrInvalidMoniker(DefaultCodespace, fmt.Sprintf("the length of the moniker must be (0,%d]", MaximumGatewayMonikerSize))
+	}
+
+	// check the details
+	if uint32(len(msg.Details)) > MaximumGatewayDetailsSize {
+		return ErrInvalidDetails(DefaultCodespace, fmt.Sprintf("the length of the details must be [0,%d]", MaximumGatewayDetailsSize))
+	}
+
+	// check the website
+	if uint32(len(msg.Website)) > MaximumGatewayWebsiteSize {
+		return ErrInvalidDetails(DefaultCodespace, fmt.Sprintf("the length of the website must be [0,%d]", MaximumGatewayWebsiteSize))
+	}
+
+	// check if the owner is included in operators
+	for _, op := range msg.Operators {
+		if op.Equals(msg.Owner) {
+			return ErrInvalidOperator(DefaultCodespace, "the owner can not be an operator")
+		}
+	}
+
 	return nil
 }
 
 // String returns the representation of the msg
 func (msg MsgEditGateway) String() string {
-	return fmt.Sprintf("MsgEditGateway{%s, %s, %s, %s, %s, %s, %v}", msg.Owner, msg.Identity, msg.Moniker, msg.Details, msg.Website, msg.RedeemAddr, msg.Operators)
+	return fmt.Sprintf("MsgEditGateway{%s, %s, %s, %s, %s, %s, %v}", msg.Owner, msg.Moniker, msg.Identity, msg.Details, msg.Website, msg.RedeemAddr, msg.Operators)
 }
 
 // GetSignBytes implements Msg
