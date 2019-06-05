1e52ff8c91ee718640ebd689c6cdab687a711de1
secret <sufay30281@gmail.com>
2019-06-05 13:25:41 +0800

    implement lcd query handlers for gateway

--- client/asset/lcd/query.go
+++ client/asset/lcd/query.go
@@ -9,19 +9,17 @@ import (
 )
 
 func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) {
-
 	// Get the gateway from a moniker
 	r.HandleFunc(
 		"/asset/gateway/{GatewayMoniker}/gateway",
 		monikerGatewayHandlerFn(cliCtx, cdc),
 	).Methods("GET")
 
-	// Get all gateways from a delegator
+	// Get all gateways from an owner
 	r.HandleFunc(
 		"/asset/gateway/{OwnerAddr}/gateways",
 		ownerGatewaysHandlerFn(cliCtx, cdc),
 	).Methods("GET")
-
 }
 
 // monikerGatewayHandlerFn is the HTTP request handler to query a gateway of the given moniker
--- client/asset/lcd/utils.go
+++ client/asset/lcd/utils.go
@@ -1,32 +1,79 @@
 package lcd
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/irisnet/irishub/app/protocol"
+	"github.com/irisnet/irishub/app/v1/asset"
 	"github.com/irisnet/irishub/client/context"
+	"github.com/irisnet/irishub/client/utils"
 	"github.com/irisnet/irishub/codec"
+	sdk "github.com/irisnet/irishub/types"
 )
-
-// contains checks if the a given query contains one of the tx types
-func contains(stringSlice []string, txType string) bool {
-	for _, word := range stringSlice {
-		if word == txType {
-			return true
-		}
-	}
-	return false
-}
 
 // queryGateway queries a gateway of the given moniker from the specified endpoint
 func queryGateway(cliCtx context.CLIContext, cdc *codec.Codec, endpoint string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO
+		vars := mux.Vars(r)
+
+		moniker := vars["Moniker"]
+		if len(moniker) == 0 || uint32(len(moniker)) > asset.MaximumGatewayMonikerSize {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("the length of the moniker must be (0,%d]", asset.MaximumGatewayMonikerSize))
+			return
+		}
+
+		params := asset.QueryGatewayParams{
+			Moniker: moniker,
+		}
+
+		bz, err := cliCtx.Codec.MarshalJSON(params)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		res, err := cliCtx.QueryWithData(
+			fmt.Sprintf("custom/%s/%s", protocol.AssetRoute, asset.QueryGateway), bz)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		utils.PostProcessResponse(w, cliCtx.Codec, res, cliCtx.Indent)
 	}
 }
 
 // queryGateways queries all gateways of an owner from the specified endpoint
 func queryGateways(cliCtx context.CLIContext, cdc *codec.Codec, endpoint string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO
+		vars := mux.Vars(r)
+
+		ownerStr := vars["owner"]
+		owner, err := sdk.AccAddressFromBech32(ownerStr)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		params := asset.QueryGatewaysParams{
+			Owner: owner,
+		}
+
+		bz, err := cliCtx.Codec.MarshalJSON(params)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		res, err := cliCtx.QueryWithData(
+			fmt.Sprintf("custom/%s/%s", protocol.AssetRoute, asset.QueryGateways), bz)
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		utils.PostProcessResponse(w, cliCtx.Codec, res, cliCtx.Indent)
 	}
 }
