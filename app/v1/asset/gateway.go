fb7d502846c69b147ef5e7267f9743a94f556ff9
secret <sufay30281@gmail.com>
2019-06-07 02:55:57 +0800

    add Gateways struct and implement stringer

--- app/v1/asset/gateway.go
+++ app/v1/asset/gateway.go
@@ -15,6 +15,19 @@ type Gateway struct {
 	Website  string         `json:"website"`  //  the external website of the gateway
 }
 
+// String implements fmt.Stringer
 func (g Gateway) String() string {
 	return fmt.Sprintf("Gateway{%s, %s, %s, %s, %s}", g.Owner, g.Moniker, g.Identity, g.Details, g.Website)
+}
+
+// Gateways is a set of gateways
+type Gateways []Gateway
+
+// String implements fmt.Stringer
+func (gs Gateways) String() string {
+	str := fmt.Sprintf("Gateways for owner %s:", gs[0].Owner)
+	for _, g := range gs {
+		str += fmt.Sprintf("\n  %s: %s: %s : %s", g.Moniker, g.Identity, g.Details, g.Website)
+	}
+	return str
 }
