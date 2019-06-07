a9fe1db5377f16cb5861c90be558e28ccda476b1
secret <sufay30281@gmail.com>
2019-06-07 16:18:39 +0800

    add gateway lcd

---
+++ client/asset/lcd/query.go
@@ -0,0 +1,33 @@
+package lcd
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/irisnet/irishub/client/context"
+	"github.com/irisnet/irishub/codec"
+)
+
+func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) {
+	// Get the gateway from a moniker
+	r.HandleFunc(
+		"/asset/gateways/{Moniker}/gateway",
+		monikerGatewayHandlerFn(cliCtx, cdc),
+	).Methods("GET")
+
+	// Get all gateways from an owner
+	r.HandleFunc(
+		"/asset/gateways/{OwnerAddr}/gateways",
+		ownerGatewaysHandlerFn(cliCtx, cdc),
+	).Methods("GET")
+}
+
+// monikerGatewayHandlerFn is the HTTP request handler to query a gateway of the given moniker
+func monikerGatewayHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec) http.HandlerFunc {
+	return queryGateway(cliCtx, cdc, "custom/asset/gateway")
+}
+
+// ownerGatewaysHandlerFn is the HTTP request handler to query all the gateways of the specifed owner
+func ownerGatewaysHandlerFn(cliCtx context.CLIContext, cdc *codec.Codec) http.HandlerFunc {
+	return queryGateways(cliCtx, cdc, "custom/asset/gateways")
+}
