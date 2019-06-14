2fd4c6d7f8879bd4de74187849c3232df5c44342
secret <sufay30281@gmail.com>
2019-06-14 19:17:20 +0800

    modify coin name

--- app/v1/asset/fees.go
+++ app/v1/asset/fees.go
@@ -32,13 +32,13 @@ func GatewayFeeHandler(ctx sdk.Context, k Keeper, owner sdk.AccAddress, moniker 
 	totalFee := sdk.NewDec(int64(gatewayBaseFee)).Quo(calcGatewayFeeFactor(moniker))
 
 	// check if the provided fee is enough
-	if fee.AmountOf(sdk.NativeTokenMinDenom).LT(totalFee.TruncateInt()) {
+	if fee.AmountOf(sdk.NativeTokenName).LT(totalFee.TruncateInt()) {
 		return ErrInsufficientFee(k.Codespace(), fmt.Sprintf("insufficient gateway create fee; expected %s, got %s", totalFee.TruncateInt(), fee.AmountOf(sdk.NativeTokenMinDenom)))
 	}
 
 	// compute tax and burned coin
-	communityTax := sdk.NewCoin(sdk.NativeTokenMinDenom, assetTaxRate.Mul(totalFee).TruncateInt())
-	burnedCoin := sdk.NewCoin(sdk.NativeTokenMinDenom, sdk.NewDec(1).Sub(assetTaxRate).Mul(totalFee).TruncateInt())
+	communityTax := sdk.NewCoin(sdk.NativeTokenName, assetTaxRate.Mul(totalFee).TruncateInt())
+	burnedCoin := sdk.NewCoin(sdk.NativeTokenName, sdk.NewDec(1).Sub(assetTaxRate).Mul(totalFee).TruncateInt())
 
 	// send community tax
 	if _, err := k.bk.SendCoins(ctx, owner, bank.CommunityTaxCoinsAccAddr, sdk.Coins{communityTax}); err != nil {
