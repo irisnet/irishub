c04cc74b7a803bcfd4b258fa3dc45797c5e639e8
zhangzhiqiang <745124335@aa.com>
2019-06-25 14:44:28 +0800

    add edit-token rest api

--- client/asset/lcd/sendtx.go
+++ client/asset/lcd/sendtx.go
@@ -23,6 +23,12 @@ func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec
 		"/asset/gateways/{moniker}",
 		editGatewayHandlerFn(cdc, cliCtx),
 	).Methods("PUT")
+
+	// edit a token
+	r.HandleFunc(
+		"/asset/token/{token-id}",
+		editTokenHandlerFn(cdc, cliCtx),
+	).Methods("PUT")
 }
 
 type createGatewayReq struct {
@@ -41,6 +47,16 @@ type editGatewayReq struct {
 	Identity *string        `json:"identity"` //  Identity of the gateway
 	Details  *string        `json:"details"`  //  Description of the gateway
 	Website  *string        `json:"website"`  //  Website of the gateway
+}
+
+type editTokenReq struct {
+	BaseTx         utils.BaseTx   `json:"base_tx"`
+	Owner          sdk.AccAddress `json:"owner"`            // owner of asset
+	SymbolAtSource string         `json:"symbol_at_source"` //  symbol_at_source of asset
+	SymbolMinAlias string         `json:"symbol_min_alias"` //  symbol_min_alias of asset
+	MaxSupply      uint64         `json:"max_supply"`
+	Mintable       *bool          `json:"mintable"` //  mintable of asset
+	Name           string         `json:"name"`
 }
 
 func createGatewayHandlerFn(cdc *codec.Codec, cliCtx context.CLIContext) http.HandlerFunc {
@@ -90,6 +106,34 @@ func editGatewayHandlerFn(cdc *codec.Codec, cliCtx context.CLIContext) http.Hand
 
 		// create the MsgEditGateway message
 		msg := asset.NewMsgEditGateway(req.Owner, moniker, req.Identity, req.Details, req.Website)
+		err = msg.ValidateBasic()
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.SendOrReturnUnsignedTx(w, cliCtx, req.BaseTx, []sdk.Msg{msg})
+	}
+}
+
+func editTokenHandlerFn(cdc *codec.Codec, cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		tokenId := vars["token-id"]
+
+		var req editTokenReq
+		err := utils.ReadPostBody(w, r, cdc, &req)
+		if err != nil {
+			return
+		}
+
+		baseReq := req.BaseTx.Sanitize()
+		if !baseReq.ValidateBasic(w, cliCtx) {
+			return
+		}
+
+		// create the MsgEditGateway message
+		msg := asset.NewMsgEditToken(req.Name, req.SymbolAtSource, req.SymbolMinAlias, tokenId, req.MaxSupply, req.Mintable, req.Owner)
 		err = msg.ValidateBasic()
 		if err != nil {
 			utils.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
