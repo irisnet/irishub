616bc09c17f6e1aaf28a9e69ae36bee0ef0f6811
secret <sufay30281@gmail.com>
2019-07-15 14:11:10 +0800

    add error codes

---
+++ app/v1/rand/errors.go
@@ -0,0 +1,25 @@
+//nolint
+package rand
+
+import (
+	sdk "github.com/irisnet/irishub/types"
+)
+
+// Random errors reserve 100 ~ 199.
+const (
+	DefaultCodespace sdk.CodespaceType = "asset"
+
+	CodeInvalidConsumer sdk.CodeType = 100
+	CodeInvalidReqID    sdk.CodeType = 101
+)
+
+//----------------------------------------
+// Random error constructors
+
+func ErrInvalidConsumer(codespace sdk.CodespaceType, msg string) sdk.Error {
+	return sdk.NewError(codespace, CodeInvalidConsumer, msg)
+}
+
+func ErrInvalidReqID(codespace sdk.CodespaceType, msg string) sdk.Error {
+	return sdk.NewError(codespace, CodeInvalidReqID, msg)
+}
