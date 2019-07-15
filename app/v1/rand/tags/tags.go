20e90a579cfa9298aa99bab9835b80af76264dae
secret <sufay30281@gmail.com>
2019-07-15 14:12:16 +0800

    add tags and cdc

---
+++ app/v1/rand/tags/tags.go
@@ -0,0 +1,14 @@
+// nolint
+package tags
+
+import (
+	sdk "github.com/irisnet/irishub/types"
+)
+
+var (
+	ActionRequestRand = []byte("request_rand")
+
+	Action   = sdk.TagAction
+	ReqId    = "request-id"
+	Consumer = "consumer"
+)
---
+++ app/v1/rand/wire.go
@@ -0,0 +1,19 @@
+package rand
+
+import (
+	"github.com/irisnet/irishub/codec"
+)
+
+// Register concrete types on codec
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgRequestRandomNumber{}, "irishub/rand/MsgRequestRand", nil)
+
+	cdc.RegisterConcrete(&Rand{}, "irishub/rand/Rand", nil)
+	cdc.RegisterConcrete(&Request{}, "irishub/rand/Request", nil)
+}
+
+var msgCdc = codec.New()
+
+func init() {
+	RegisterCodec(msgCdc)
+}
