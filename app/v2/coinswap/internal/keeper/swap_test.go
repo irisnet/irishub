fe5ec93dfdf425e949704e99e1651331e0c17e05
zhangzhiqiang <745124335@qq.com>
2019-08-08 13:42:42 +0800

    add unit test

--- app/v2/coinswap/internal/keeper/swap_test.go
+++ app/v2/coinswap/internal/keeper/swap_test.go
@@ -1,10 +1,9 @@
 package keeper
 
 import (
-	"github.com/irisnet/irishub/app/v2/coinswap/internal/types"
-	"testing"
-
+	"fmt"
 	"github.com/stretchr/testify/require"
+	"testing"
 
 	sdk "github.com/irisnet/irishub/types"
 )
@@ -63,28 +62,94 @@ func TestGetReservePoolName(t *testing.T) {
 	}
 }
 
+type Data struct {
+	delta sdk.Int
+	x     sdk.Int
+	y     sdk.Int
+	fee   sdk.Rat
+}
+type SwapCase struct {
+	data   Data
+	expect sdk.Int
+}
+
 func TestGetInputPrice(t *testing.T) {
-	inputAmt := sdk.NewInt(100)
-	inputReserve := sdk.NewInt(1000)
-	outputReserve := sdk.NewInt(1000)
-	fee := types.DefaultParams().Fee // fee=0.003
-	amount := GetInputPrice(inputAmt, inputReserve, outputReserve, fee)
-	require.Equal(t, "90", amount.String())
+	var datas = []SwapCase{
+		{
+			data:   Data{delta: sdk.NewInt(100), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.NewRat(3, 1000)},
+			expect: sdk.NewInt(90),
+		},
+		{
+			data:   Data{delta: sdk.NewInt(200), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.NewRat(3, 1000)},
+			expect: sdk.NewInt(166),
+		},
+		{
+			data:   Data{delta: sdk.NewInt(300), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.NewRat(3, 1000)},
+			expect: sdk.NewInt(230),
+		},
+		{
+			data:   Data{delta: sdk.NewInt(1000), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.NewRat(3, 1000)},
+			expect: sdk.NewInt(499),
+		},
+		{
+			data:   Data{delta: sdk.NewInt(1000), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.ZeroRat()},
+			expect: sdk.NewInt(500),
+		},
+	}
+	for _, tcase := range datas {
+		data := tcase.data
+		actual := GetInputPrice(data.delta, data.x, data.y, data.fee)
+		fmt.Println(fmt.Sprintf("expect:%s,actual:%s", tcase.expect.String(), actual.String()))
+		require.Equal(t, tcase.expect, actual)
+	}
 }
 
 func TestGetOutputPrice(t *testing.T) {
-	inputAmt := sdk.NewInt(100)
-	inputReserve := sdk.NewInt(1000)
-	outputReserve := sdk.NewInt(1000)
-	fee := types.DefaultParams().Fee // fee=0.003
-	amount := GetOutputPrice(inputAmt, inputReserve, outputReserve, fee)
-	require.Equal(t, "90", amount.String())
+	var datas = []SwapCase{
+		{
+			data:   Data{delta: sdk.NewInt(100), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.NewRat(3, 1000)},
+			expect: sdk.NewInt(112),
+		},
+		{
+			data:   Data{delta: sdk.NewInt(200), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.NewRat(3, 1000)},
+			expect: sdk.NewInt(251),
+		},
+		{
+			data:   Data{delta: sdk.NewInt(300), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.NewRat(3, 1000)},
+			expect: sdk.NewInt(430),
+		},
+		{
+			data:   Data{delta: sdk.NewInt(300), x: sdk.NewInt(1000), y: sdk.NewInt(1000), fee: sdk.ZeroRat()},
+			expect: sdk.NewInt(429),
+		},
+	}
+	for _, tcase := range datas {
+		data := tcase.data
+		actual := GetOutputPrice(data.delta, data.x, data.y, data.fee)
+		fmt.Println(fmt.Sprintf("expect:%s,actual:%s", tcase.expect.String(), actual.String()))
+		require.Equal(t, tcase.expect, actual)
+	}
 }
 
-func TestSwapByInput(t *testing.T) {
-
-}
-
-func TestSwapByOutput(t *testing.T) {
-
-}
+//func TestSwapByInput(t *testing.T) {
+//	ctx, keeper, accs := createTestInput(t, sdk.NewInt(100000000), 1)
+//	sender := accs[0].GetAddress()
+//
+//	depositCoin := sdk.NewCoin("btc-min",sdk.NewInt(1000))
+//	depositAmount := sdk.NewInt(1000)
+//	minReward := sdk.NewInt(1)
+//	deadline := time.Now().Add(1 * time.Minute)
+//	msg := types.NewMsgAddLiquidity(depositCoin,depositAmount,minReward,deadline,sender)
+//	err := keeper.AddLiquidity(ctx,msg)
+//	require.Nil(t,err)
+//
+//	exactSoldCoin := sdk.NewCoin("btc-min",sdk.NewInt(100))
+//	minExpect := sdk.NewCoin(sdk.IrisAtto,sdk.NewInt(100))
+//	reward,err := keeper.SwapByInput(ctx,exactSoldCoin,minExpect,accs[0].GetAddress(),nil)
+//	require.Nil(t,err)
+//	require.Equal(t,sdk.NewInt(90),reward)
+//}
+//
+//func TestSwapByOutput(t *testing.T) {
+//
+//}
