6972c789a7f8eeaf3502e66c7d7a3033b283bbfd
Yelong Zhang <yelong@bianjie.ai>
2019-08-14 10:16:20 +0800

    Update app/v2/coinswap/internal/types/utils.go
    Co-Authored-By: Zhiqiang Zhang <zhiqiang@bianjie.ai>

--- app/v2/coinswap/internal/types/utils.go
+++ app/v2/coinswap/internal/types/utils.go
@@ -18,7 +18,10 @@ func GetUniId(denom1, denom2 string) (string, sdk.Error) {
 		return "", ErrIllegalDenom(fmt.Sprintf("illegal denomnations for forming uni id, must have one native denom: %s", sdk.IrisAtto))
 	}
 
-	coinName, err := sdk.GetCoinNameByDenom(denom2)
+        denom := denom1
+        if denom != sdk.IrisAtto
+            denom = denom2
+	coinName, err := sdk.GetCoinNameByDenom(denom)
 	if err != nil {
 		return "", ErrIllegalDenom(err.Error())
 	}
