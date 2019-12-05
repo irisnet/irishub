64678da0bc843321625d06a7c39bb73b46c7a3a4
secret <sufay30281@gmail.com>
2019-12-05 15:55:59 +0800

    refactor querier

--- modules/service/internal/keeper/querier.go
+++ modules/service/internal/keeper/querier.go
@@ -1,35 +1,28 @@
 package keeper
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/irisnet/irishub/modules/service/internal/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
-const (
-	QueryDefinition = "definition"
-	QueryBinding    = "binding"
-	QueryBindings   = "bindings"
-	QueryRequests   = "requests"
-	QueryResponse   = "response"
-	QueryFees       = "fees"
-)
-
 func NewQuerier(k Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, sdk.Error) {
 		switch path[0] {
-		case QueryDefinition:
+		case types.QueryDefinition:
 			return queryDefinition(ctx, req, k)
-		case QueryBinding:
+		case types.QueryBinding:
 			return queryBinding(ctx, req, k)
-		case QueryBindings:
+		case types.QueryBindings:
 			return queryBindings(ctx, req, k)
-		case QueryRequests:
+		case types.QueryRequests:
 			return queryRequests(ctx, req, k)
-		case QueryResponse:
+		case types.QueryResponse:
 			return queryResponse(ctx, req, k)
-		case QueryFees:
+		case types.QueryFees:
 			return queryFees(ctx, req, k)
 		default:
 			return nil, sdk.ErrUnknownRequest("unknown service query endpoint")
@@ -37,172 +30,154 @@ func NewQuerier(k Keeper) sdk.Querier {
 	}
 }
 
-type QueryServiceParams struct {
-	DefChainID  string
-	ServiceName string
-}
-
-type DefinitionOutput struct {
-	Definition types.SvcDef           `json:"definition"`
-	Methods    []types.MethodProperty `json:"methods"`
-}
-
-func queryDefinition(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
-	var params QueryServiceParams
-	err := k.cdc.UnmarshalJSON(req.Data, &params)
+func queryDefinition(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+	var params types.QueryServiceParams
+	err := keeper.cdc.UnmarshalJSON(req.Data, &params)
 	if err != nil {
-		return nil, sdk.ParseParamsErr(err)
+		return nil, sdk.ErrInternal(fmt.Sprintf("failed to parse params: %s", err))
 	}
-	svcDef, found := k.GetServiceDefinition(ctx, params.DefChainID, params.ServiceName)
+
+	svcDef, found := keeper.GetServiceDefinition(ctx, params.DefChainID, params.ServiceName)
 	if !found {
 		return nil, types.ErrSvcDefNotExists(types.DefaultCodespace, params.DefChainID, params.ServiceName)
 	}
 
-	iterator := k.GetMethods(ctx, params.DefChainID, params.ServiceName)
+	iterator := keeper.GetMethods(ctx, params.DefChainID, params.ServiceName)
 	defer iterator.Close()
+
 	var methods []types.MethodProperty
 	for ; iterator.Valid(); iterator.Next() {
 		var method types.MethodProperty
-		k.cdc.MustUnmarshalBinaryLengthPrefixed(iterator.Value(), &method)
+		keeper.cdc.MustUnmarshalBinaryLengthPrefixed(iterator.Value(), &method)
 		methods = append(methods, method)
 	}
 
-	definitionOutput := DefinitionOutput{Definition: svcDef, Methods: methods}
+	definitionOutput := types.DefinitionOutput{Definition: svcDef, Methods: methods}
 
-	bz, err := codec.MarshalJSONIndent(k.cdc, definitionOutput)
+	bz, err := codec.MarshalJSONIndent(keeper.cdc, definitionOutput)
 	if err != nil {
-		return nil, sdk.MarshalResultErr(err)
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
 	}
+
 	return bz, nil
 }
 
-type QueryBindingParams struct {
-	DefChainID  string
-	ServiceName string
-	BindChainId string
-	Provider    sdk.AccAddress
-}
-
-func queryBinding(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
-	var params QueryBindingParams
-	err := k.cdc.UnmarshalJSON(req.Data, &params)
+func queryBinding(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+	var params types.QueryBindingParams
+	err := keeper.cdc.UnmarshalJSON(req.Data, &params)
 	if err != nil {
-		return nil, sdk.ParseParamsErr(err)
+		return nil, sdk.ErrInternal(fmt.Sprintf("failed to parse params: %s", err))
 	}
-	svcBinding, found := k.GetServiceBinding(ctx, params.DefChainID, params.ServiceName, params.BindChainId, params.Provider)
+
+	svcBinding, found := keeper.GetServiceBinding(ctx, params.DefChainID, params.ServiceName, params.BindChainId, params.Provider)
 	if !found {
 		return nil, types.ErrSvcBindingNotExists(types.DefaultCodespace)
 	}
-	bz, err := codec.MarshalJSONIndent(k.cdc, svcBinding)
+
+	bz, err := codec.MarshalJSONIndent(keeper.cdc, svcBinding)
 	if err != nil {
-		return nil, sdk.MarshalResultErr(err)
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
 	}
+
 	return bz, nil
 }
 
-func queryBindings(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
-	var params QueryServiceParams
-	err := k.cdc.UnmarshalJSON(req.Data, &params)
+func queryBindings(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+	var params types.QueryServiceParams
+	err := keeper.cdc.UnmarshalJSON(req.Data, &params)
 	if err != nil {
-		return nil, sdk.ParseParamsErr(err)
+		return nil, sdk.ErrInternal(fmt.Sprintf("failed to parse params: %s", err))
 	}
 
-	iterator := k.ServiceBindingsIterator(ctx, params.DefChainID, params.ServiceName)
+	iterator := keeper.ServiceBindingsIterator(ctx, params.DefChainID, params.ServiceName)
 	defer iterator.Close()
+
 	var bindings []types.SvcBinding
 	for ; iterator.Valid(); iterator.Next() {
 		var binding types.SvcBinding
-		k.cdc.MustUnmarshalBinaryLengthPrefixed(iterator.Value(), &binding)
+		keeper.cdc.MustUnmarshalBinaryLengthPrefixed(iterator.Value(), &binding)
 		bindings = append(bindings, binding)
 	}
 
-	bz, err := codec.MarshalJSONIndent(k.cdc, bindings)
+	bz, err := codec.MarshalJSONIndent(keeper.cdc, bindings)
 	if err != nil {
-		return nil, sdk.MarshalResultErr(err)
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
 	}
+
 	return bz, nil
 }
 
-func queryRequests(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
-	var params QueryBindingParams
-	err := k.cdc.UnmarshalJSON(req.Data, &params)
+func queryRequests(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+	var params types.QueryBindingParams
+	err := keeper.cdc.UnmarshalJSON(req.Data, &params)
 	if err != nil {
-		return nil, sdk.ParseParamsErr(err)
+		return nil, sdk.ErrInternal(fmt.Sprintf("failed to parse params: %s", err))
 	}
 
-	iterator := k.ActiveBindRequestsIterator(ctx, params.DefChainID, params.ServiceName, params.BindChainId, params.Provider)
+	iterator := keeper.ActiveBindRequestsIterator(ctx, params.DefChainID, params.ServiceName, params.BindChainId, params.Provider)
 	defer iterator.Close()
+
 	var requests []types.SvcRequest
 	for ; iterator.Valid(); iterator.Next() {
 		var request types.SvcRequest
-		k.cdc.MustUnmarshalBinaryLengthPrefixed(iterator.Value(), &request)
+		keeper.cdc.MustUnmarshalBinaryLengthPrefixed(iterator.Value(), &request)
 		requests = append(requests, request)
 	}
 
-	bz, err := codec.MarshalJSONIndent(k.cdc, requests)
+	bz, err := codec.MarshalJSONIndent(keeper.cdc, requests)
 	if err != nil {
-		return nil, sdk.MarshalResultErr(err)
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
 	}
+
 	return bz, nil
 }
 
-type QueryResponseParams struct {
-	ReqChainId string
-	RequestId  string
-}
-
-func queryResponse(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
-	var params QueryResponseParams
-	err := k.cdc.UnmarshalJSON(req.Data, &params)
+func queryResponse(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+	var params types.QueryResponseParams
+	err := keeper.cdc.UnmarshalJSON(req.Data, &params)
 	if err != nil {
-		return nil, sdk.ParseParamsErr(err)
+		return nil, sdk.ErrInternal(fmt.Sprintf("failed to parse params: %s", err))
 	}
 
 	eHeight, rHeight, counter, err := types.ConvertRequestID(params.RequestId)
 	if err != nil {
 		return nil, sdk.ErrUnknownRequest(err.Error())
 	}
-	response, found := k.GetResponse(ctx, params.ReqChainId, eHeight, rHeight, counter)
+
+	response, found := keeper.GetResponse(ctx, params.ReqChainId, eHeight, rHeight, counter)
 	if !found {
 		return nil, types.ErrNoResponseFound(types.DefaultCodespace, params.RequestId)
 	}
 
-	bz, err := codec.MarshalJSONIndent(k.cdc, response)
+	bz, err := codec.MarshalJSONIndent(keeper.cdc, response)
 	if err != nil {
-		return nil, sdk.MarshalResultErr(err)
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
 	}
+
 	return bz, nil
 }
 
-type QueryFeesParams struct {
-	Address sdk.AccAddress
-}
-
-type FeesOutput struct {
-	ReturnedFee sdk.Coins `json:"returned_fee"`
-	IncomingFee sdk.Coins `json:"incoming_fee"`
-}
-
-func queryFees(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
-	var params QueryFeesParams
-	err := k.cdc.UnmarshalJSON(req.Data, &params)
+func queryFees(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+	var params types.QueryFeesParams
+	err := keeper.cdc.UnmarshalJSON(req.Data, &params)
 	if err != nil {
-		return nil, sdk.ParseParamsErr(err)
+		return nil, sdk.ErrInternal(fmt.Sprintf("failed to parse params: %s", err))
 	}
 
-	var feesOutput FeesOutput
+	var feesOutput types.FeesOutput
 
-	if returnFee, found := k.GetReturnFee(ctx, params.Address); found {
+	if returnFee, found := keeper.GetReturnFee(ctx, params.Address); found {
 		feesOutput.ReturnedFee = returnFee.Coins
 	}
 
-	if incomingFee, found := k.GetIncomingFee(ctx, params.Address); found {
+	if incomingFee, found := keeper.GetIncomingFee(ctx, params.Address); found {
 		feesOutput.IncomingFee = incomingFee.Coins
 	}
 
-	bz, err := codec.MarshalJSONIndent(k.cdc, feesOutput)
+	bz, err := codec.MarshalJSONIndent(keeper.cdc, feesOutput)
 	if err != nil {
-		return nil, sdk.MarshalResultErr(err)
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("could not marshal result to JSON", err.Error()))
 	}
+
 	return bz, nil
 }
---
+++ modules/service/internal/types/querier.go
@@ -0,0 +1,45 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const (
+	QueryDefinition = "definition"
+	QueryBinding    = "binding"
+	QueryBindings   = "bindings"
+	QueryRequests   = "requests"
+	QueryResponse   = "response"
+	QueryFees       = "fees"
+)
+
+type QueryServiceParams struct {
+	DefChainID  string
+	ServiceName string
+}
+
+type DefinitionOutput struct {
+	Definition SvcDef           `json:"definition"`
+	Methods    []MethodProperty `json:"methods"`
+}
+
+type QueryBindingParams struct {
+	DefChainID  string
+	ServiceName string
+	BindChainId string
+	Provider    sdk.AccAddress
+}
+
+type QueryResponseParams struct {
+	ReqChainId string
+	RequestId  string
+}
+
+type QueryFeesParams struct {
+	Address sdk.AccAddress
+}
+
+type FeesOutput struct {
+	ReturnedFee sdk.Coins `json:"returned_fee"`
+	IncomingFee sdk.Coins `json:"incoming_fee"`
+}
