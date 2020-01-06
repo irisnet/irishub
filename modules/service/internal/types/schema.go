40dcfcea2f8fe838a33dabdb711d3ed5add34c01
secret <sufay30281@gmail.com>
2020-01-06 12:03:51 +0800

    implement schema handling

--- modules/service/internal/types/schema.go
+++ modules/service/internal/types/schema.go
@@ -1,21 +1,40 @@
 package types
 
-// ServiceSchema defines the service schema fields which are required
-type ServiceSchema struct {
-	Input  map[string]interface{}
-	Output map[string]interface{}
-	Error  map[string]interface{}
+import (
+	"encoding/json"
+
+	"github.com/xeipuuv/gojsonschema"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// ServiceSchemas defines the service schemas
+type ServiceSchemas struct {
+	Input  map[string]interface{} `json:"input"`
+	Output map[string]interface{} `json:"output"`
+	Error  map[string]interface{} `json:"error"`
 }
 
-// ValidateServiceSchema checks if the given schema is valid
-func ValidateServiceSchema(schema string) error {
+// ValidateServiceSchemas validates the given service schemas
+func ValidateServiceSchemas(schemas string) error {
+	var svcSchemas ServiceSchemas
+	if err := json.Unmarshal([]byte(schemas), &svcSchemas); err != nil {
+		return sdkerrors.Wrapf(ErrInvalidSchemas, "failed to unmarshal the schemas: %s", err)
+	}
+
+	if err := validateInputSchema(svcSchemas.Input); err != nil {
+		return err
+	}
+
+	if err := validateOutputSchema(svcSchemas.Output); err != nil {
+		return err
+	}
+
+	if err := validateErrorSchema(svcSchemas.Error); err != nil {
+		return err
+	}
+
 	return nil
-}
-
-// ParseServiceSchema parses the given schema
-func ParseServiceSchema(schema string) (ServiceSchema, error) {
-	// TODO
-	return ServiceSchema{}, nil
 }
 
 // ValidateBindingPricing checks if the given pricing is valid
@@ -24,20 +43,137 @@ func ValidateBindingPricing(pricing string) error {
 	return nil
 }
 
-// ValidateRequestInput validates the request input against the given schema
-func ValidateRequestInput(schema string, input string) error {
-	// TODO
+// ValidateRequestInput validates the request input against the input schema.
+// assume that the schemas is valid.
+func ValidateRequestInput(schemas string, input string) error {
+	inputSchemaBz := parseInputSchema(schemas)
+	inputSchemaLoader := gojsonschema.NewBytesLoader(inputSchemaBz)
+
+	inputLoader := gojsonschema.NewStringLoader(input)
+
+	if _, err := gojsonschema.Validate(inputSchemaLoader, inputLoader); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// ValidateResponseOutput validates the response output against the given schema
-func ValidateResponseOutput(schema string, output string) error {
-	// TODO
+// ValidateResponseOutput validates the response output against the output schema.
+// assume that the schemas is valid.
+func ValidateResponseOutput(schemas string, output string) error {
+	outputSchemaBz := parseOutputSchema(schemas)
+	outputSchemaLoader := gojsonschema.NewBytesLoader(outputSchemaBz)
+
+	outputLoader := gojsonschema.NewStringLoader(output)
+
+	if _, err := gojsonschema.Validate(outputSchemaLoader, outputLoader); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// ValidateResponseError validates the response err against the given schema
-func ValidateResponseError(schema string, err string) error {
-	// TODO
+// ValidateResponseError validates the response err against the error schema.
+// assume that the schemas is valid.
+func ValidateResponseError(schemas string, err string) error {
+	errSchemaBz := parseErrorSchema(schemas)
+	errSchemaLoader := gojsonschema.NewBytesLoader(errSchemaBz)
+
+	errLoader := gojsonschema.NewStringLoader(err)
+
+	if _, err := gojsonschema.Validate(errSchemaLoader, errLoader); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func validateInputSchema(inputSchema map[string]interface{}) error {
+	inputSchemaBz, err := json.Marshal(inputSchema)
+	if err != nil {
+		return sdkerrors.Wrapf(ErrInvalidSchemas, "invalid input schema: %s", err)
+	}
+
+	_, err = gojsonschema.NewSchema(gojsonschema.NewBytesLoader(inputSchemaBz))
+	if err != nil {
+		return sdkerrors.Wrapf(ErrInvalidSchemas, "invalid input schema: %s", err)
+	}
+
+	return nil
+}
+
+func validateOutputSchema(outputSchema map[string]interface{}) error {
+	outputSchemaBz, err := json.Marshal(outputSchema)
+	if err != nil {
+		return sdkerrors.Wrapf(ErrInvalidSchemas, "invalid output schema: %s", err)
+	}
+
+	_, err = gojsonschema.NewSchema(gojsonschema.NewBytesLoader(outputSchemaBz))
+	if err != nil {
+		return sdkerrors.Wrapf(ErrInvalidSchemas, "invalid output schema: %s", err)
+	}
+
+	return nil
+}
+
+func validateErrorSchema(errSchema map[string]interface{}) error {
+	errSchemaBz, err := json.Marshal(errSchema)
+	if err != nil {
+		return sdkerrors.Wrapf(ErrInvalidSchemas, "invalid error schema: %s", err)
+	}
+
+	_, err = gojsonschema.NewSchema(gojsonschema.NewBytesLoader(errSchemaBz))
+	if err != nil {
+		return sdkerrors.Wrapf(ErrInvalidSchemas, "invalid error schema: %s", err)
+	}
+
+	return nil
+}
+
+// parseInputSchema parses the input schema from the given schemas
+// assume that the schemas is valid. Panic if invalid
+func parseInputSchema(schemas string) []byte {
+	var svcSchemas ServiceSchemas
+	if err := json.Unmarshal([]byte(schemas), &svcSchemas); err != nil {
+		panic(err)
+	}
+
+	inputSchemaBz, err := json.Marshal(svcSchemas.Input)
+	if err != nil {
+		panic(err)
+	}
+
+	return inputSchemaBz
+}
+
+// parseOutputSchema parses the output schema from the given schemas
+// assume that the schemas is valid. Panic if invalid
+func parseOutputSchema(schemas string) []byte {
+	var svcSchemas ServiceSchemas
+	if err := json.Unmarshal([]byte(schemas), &svcSchemas); err != nil {
+		panic(err)
+	}
+
+	outputSchemaBz, err := json.Marshal(svcSchemas.Output)
+	if err != nil {
+		panic(err)
+	}
+
+	return outputSchemaBz
+}
+
+// parseErrorSchema parses the error schema from the given schemas
+// assume that the schemas is valid. Panic if invalid
+func parseErrorSchema(schemas string) []byte {
+	var svcSchemas ServiceSchemas
+	if err := json.Unmarshal([]byte(schemas), &svcSchemas); err != nil {
+		panic(err)
+	}
+
+	errSchemaBz, err := json.Marshal(svcSchemas.Error)
+	if err != nil {
+		panic(err)
+	}
+
+	return errSchemaBz
 }
