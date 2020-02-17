0190331b57c0a345f425fdf8086bdabf22698c1e
secret2830 <sufay30281@gmail.com>
2020-02-17 18:54:52 +0800

    modify exported

--- app/v3/service/exported/exported.go
+++ app/v3/service/exported/exported.go
@@ -4,6 +4,21 @@ import (
 	"github.com/irisnet/irishub/app/v3/service/internal/types"
 )
 
-type RequestContext types.RequestContext
-type RequestContextState types.RequestContextState
-type ResponseCallback types.ResponseCallback
+type (
+	// RequestContext defines a context which holds request-related data
+	RequestContext types.RequestContext
+
+	// RequestContextState represents the request context state
+	RequestContextState types.RequestContextState
+
+	// ResponseCallback defines the response callback interface
+	ResponseCallback types.ResponseCallback
+)
+
+const (
+	// RUNNING means the request context is running
+	RUNNING = types.RUNNING
+
+	// PAUSED means the request context is paused
+	PAUSED = types.PAUSED
+)
