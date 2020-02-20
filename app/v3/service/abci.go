cc51e81afc709ab7ae55098bfcb6e0fa9643ea2e
secret2830 <sufay30281@gmail.com>
2020-02-21 02:48:39 +0800

    fix EndBlocker

--- app/v3/service/abci.go
+++ app/v3/service/abci.go
@@ -28,56 +28,55 @@ func EndBlocker(ctx sdk.Context, k Keeper) (resTags sdk.Tags) {
 		k.GetCdc().MustUnmarshalBinaryLengthPrefixed(expiredReqBatchIterator.Value(), &reqContextID)
 
 		reqContext, _ := k.GetRequestContext(ctx, reqContextID)
-		if reqContext.BatchState == BATCHCOMPLETED {
-			continue
-		}
 
-		// iterate active requests
-		activeReqIterator := k.ActiveRequestsIteratorByReqCtx(ctx, reqContextID, reqContext.BatchCounter)
-		defer activeReqIterator.Close()
+		if reqContext.BatchState != BATCHCOMPLETED {
+			// iterate active requests
+			activeReqIterator := k.ActiveRequestsIteratorByReqCtx(ctx, reqContextID, reqContext.BatchCounter)
+			defer activeReqIterator.Close()
 
-		for ; activeReqIterator.Valid(); activeReqIterator.Next() {
-			requestID := activeReqIterator.Value()
-			k.GetCdc().MustUnmarshalBinaryLengthPrefixed(activeReqIterator.Value(), &requestID)
+			for ; activeReqIterator.Valid(); activeReqIterator.Next() {
+				requestID := activeReqIterator.Value()
+				k.GetCdc().MustUnmarshalBinaryLengthPrefixed(activeReqIterator.Value(), &requestID)
 
-			request, _ := k.GetRequest(ctx, requestID)
+				request, _ := k.GetRequest(ctx, requestID)
 
-			if !request.Profiling {
-				binding, found := k.GetServiceBinding(ctx, request.ServiceName, request.Provider)
-				if found {
-					slashedCoins := sdk.NewCoins()
+				if !request.Profiling {
+					binding, found := k.GetServiceBinding(ctx, request.ServiceName, request.Provider)
+					if found {
+						slashedCoins := sdk.NewCoins()
 
-					for _, coin := range binding.Deposit {
-						taxAmount := sdk.NewDecFromInt(coin.Amount).Mul(slashFraction).TruncateInt()
-						slashedCoins.Add(sdk.NewCoins(sdk.NewCoin(coin.Denom, taxAmount)))
+						for _, coin := range binding.Deposit {
+							taxAmount := sdk.NewDecFromInt(coin.Amount).Mul(slashFraction).TruncateInt()
+							slashedCoins.Add(sdk.NewCoins(sdk.NewCoin(coin.Denom, taxAmount)))
+						}
+
+						if err := k.Slash(ctx, binding, slashedCoins); err != nil {
+							panic(err)
+						}
+
+						if err := k.RefundServiceFee(ctx, request.Consumer, request.ServiceFee); err != nil {
+							panic(err)
+						}
 					}
+				}
 
-					if err := k.Slash(ctx, binding, slashedCoins); err != nil {
-						panic(err)
-					}
+				k.DeleteActiveRequest(ctx, reqContext.ServiceName, request.Provider, ctx.BlockHeight(), requestID)
+				k.DeleteActiveRequestByID(ctx, requestID)
 
-					if err := k.RefundServiceFee(ctx, request.Consumer, request.ServiceFee); err != nil {
-						panic(err)
-					}
+				k.GetMetrics().ActiveRequests.Add(-1)
+			}
+
+			// callback
+			if len(reqContext.ModuleName) != 0 {
+				if reqContext.BatchResponseCount >= reqContext.ResponseThreshold {
+					respCallback, _ := k.GetResponseCallback(reqContext.ModuleName)
+					respCallback(ctx, reqContextID, k.GetResponsesOutput(ctx, reqContextID, reqContext.BatchCounter))
 				}
 			}
 
-			k.DeleteActiveRequest(ctx, reqContext.ServiceName, request.Provider, ctx.BlockHeight(), requestID)
-			k.DeleteActiveRequestByID(ctx, requestID)
-
-			k.GetMetrics().ActiveRequests.Add(-1)
+			reqContext.BatchState = BATCHCOMPLETED
+			k.SetRequestContext(ctx, reqContextID, reqContext)
 		}
-
-		// callback
-		if len(reqContext.ModuleName) != 0 {
-			if reqContext.BatchResponseCount >= reqContext.ResponseThreshold {
-				respCallback, _ := k.GetResponseCallback(reqContext.ModuleName)
-				respCallback(ctx, reqContextID, k.GetResponsesOutput(ctx, reqContextID, reqContext.BatchCounter))
-			}
-		}
-
-		reqContext.BatchState = BATCHCOMPLETED
-		k.SetRequestContext(ctx, reqContextID, reqContext)
 
 		k.DeleteRequestBatchExpiration(ctx, reqContextID, ctx.BlockHeight())
 
@@ -96,21 +95,19 @@ func EndBlocker(ctx sdk.Context, k Keeper) (resTags sdk.Tags) {
 
 		reqContext, _ := k.GetRequestContext(ctx, reqContextID)
 
-		if reqContext.State != RUNNING {
-			continue
-		}
+		if reqContext.State == RUNNING {
+			providers, totalPrices := k.FilterServiceProviders(ctx, reqContext.ServiceName, reqContext.Providers, reqContext.ServiceFeeCap)
+			if len(reqContext.ModuleName) == 0 || len(providers) >= int(reqContext.ResponseThreshold) {
+				if err := k.DeductServiceFees(ctx, reqContext.Consumer, totalPrices); err != nil {
+					reqContext.State = PAUSED
+				}
 
-		providers, totalPrices := k.FilterServiceProviders(ctx, reqContext.ServiceName, reqContext.Providers, reqContext.ServiceFeeCap)
-		if len(reqContext.ModuleName) == 0 || len(providers) >= int(reqContext.ResponseThreshold) {
-			if err := k.DeductServiceFees(ctx, reqContext.Consumer, totalPrices); err != nil {
-				reqContext.State = PAUSED
+				reqContext.BatchCounter++
+				k.SetRequestContext(ctx, reqContextID, reqContext)
+
+				k.InitiateRequests(ctx, reqContextID, providers)
+				k.AddRequestBatchExpiration(ctx, reqContextID, ctx.BlockHeight()+reqContext.Timeout)
 			}
-
-			reqContext.BatchCounter++
-			k.SetRequestContext(ctx, reqContextID, reqContext)
-
-			k.InitiateRequests(ctx, reqContextID, providers)
-			k.AddRequestBatchExpiration(ctx, reqContextID, ctx.BlockHeight()+reqContext.Timeout)
 		}
 
 		k.DeleteNewRequestBatch(ctx, reqContextID, ctx.BlockHeight())
