57b54e7d997fd206a327930a94bce9a438658eb6
secret2830 <sufay30281@gmail.com>
2020-02-23 15:19:50 +0800

    genesis handling

--- app/v3/service/internal/types/genesis.go
+++ app/v3/service/internal/types/genesis.go
@@ -1,18 +1,35 @@
 package types
 
+import "encoding/hex"
+
 // GenesisState - all service state that must be provided at genesis
 type GenesisState struct {
-	Params Params `json:"params"` // service params
+	Params          Params                    `json:"params"`           // service params
+	RequestContexts map[string]RequestContext `json:"request_contexts"` // request contexts
+	Requests        map[string]CompactRequest `json:"requests"`         // requests
+	Responses       map[string]Response       `json:"responses"`        // responses
 }
 
 // NewGenesisState constructs a GenesisState
-func NewGenesisState(params Params) GenesisState {
-	return GenesisState{Params: params}
+func NewGenesisState(
+	params Params,
+	requestContexts map[string]RequestContext,
+	requests map[string]CompactRequest,
+	reponses map[string]Response,
+) GenesisState {
+	return GenesisState{
+		Params:          params,
+		RequestContexts: requestContexts,
+		Requests:        requests,
+		Responses:       reponses,
+	}
 }
 
 // DefaultGenesisState returns the default genesis state
 func DefaultGenesisState() GenesisState {
-	return GenesisState{Params: DefaultParams()}
+	return GenesisState{
+		Params: DefaultParams(),
+	}
 }
 
 // get raw genesis raw message for testing
@@ -29,5 +46,24 @@ func ValidateGenesis(data GenesisState) error {
 	if err != nil {
 		return err
 	}
+
+	for requestContextID := range data.RequestContexts {
+		if _, err := hex.DecodeString(requestContextID); err != nil {
+			return err
+		}
+	}
+
+	for requestID := range data.Requests {
+		if _, err := ConvertRequestID(requestID); err != nil {
+			return err
+		}
+	}
+
+	for requestID := range data.Responses {
+		if _, err := ConvertRequestID(requestID); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
