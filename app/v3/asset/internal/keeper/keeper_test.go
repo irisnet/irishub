3d596034513df0e2cf37e36aa9d5f8ddb6410e20
secret <sufay30281@gmail.com>
2020-03-11 10:35:03 +0800

    fix tests

--- app/v3/asset/internal/keeper/keeper_test.go
+++ app/v3/asset/internal/keeper/keeper_test.go
@@ -41,7 +41,7 @@ func TestKeeperIssueToken(t *testing.T) {
 	assert.True(t, keeper.HasToken(ctx, msg.Symbol))
 
 	token, err := keeper.getToken(ctx, msg.Symbol)
-	ft := types.NewFungibleToken(msg.Symbol, msg.Name, msg.MinUnitAlias, msg.Decimal, sdk.NewIntWithDecimal(int64(msg.InitialSupply), int(msg.Decimal)), sdk.NewIntWithDecimal(int64(msg.MaxSupply), int(msg.Decimal)), msg.Mintable, msg.Owner)
+	ft := types.NewFungibleToken(msg.Symbol, msg.Name, msg.MinUnitAlias, msg.Decimal, msg.InitialSupply, msg.MaxSupply, msg.Mintable, msg.Owner)
 	assert.NoError(t, err)
 	assert.Equal(t, ft, token)
 }
@@ -71,7 +71,7 @@ func TestKeeperEditToken(t *testing.T) {
 	assert.True(t, keeper.HasToken(ctx, msg.Symbol))
 
 	token, err := keeper.getToken(ctx, msg.Symbol)
-	ft := types.NewFungibleToken(msg.Symbol, msg.Name, msg.MinUnitAlias, msg.Decimal, sdk.NewIntWithDecimal(int64(msg.InitialSupply), int(msg.Decimal)), sdk.NewIntWithDecimal(int64(msg.MaxSupply), int(msg.Decimal)), msg.Mintable, msg.Owner)
+	ft := types.NewFungibleToken(msg.Symbol, msg.Name, msg.MinUnitAlias, msg.Decimal, msg.InitialSupply, msg.MaxSupply, msg.Mintable, msg.Owner)
 	assert.NoError(t, err)
 	assert.Equal(t, ft, token)
 
@@ -120,7 +120,7 @@ func TestMintToken(t *testing.T) {
 	assert.True(t, keeper.HasToken(ctx, msg.Symbol))
 
 	token, err := keeper.getToken(ctx, msg.Symbol)
-	ft := types.NewFungibleToken(msg.Symbol, msg.Name, msg.MinUnitAlias, msg.Decimal, sdk.NewIntWithDecimal(int64(msg.InitialSupply), int(msg.Decimal)), sdk.NewIntWithDecimal(int64(msg.MaxSupply), int(msg.Decimal)), msg.Mintable, msg.Owner)
+	ft := types.NewFungibleToken(msg.Symbol, msg.Name, msg.MinUnitAlias, msg.Decimal, msg.InitialSupply, msg.MaxSupply, msg.Mintable, msg.Owner)
 	assert.NoError(t, err)
 	assert.Equal(t, ft, token)
 
@@ -158,7 +158,7 @@ func TestTransferOwnerKeeper(t *testing.T) {
 	assert.True(t, keeper.HasToken(ctx, issueMsg.Symbol))
 
 	tokenSrc, err := keeper.getToken(ctx, issueMsg.Symbol)
-	ft := types.NewFungibleToken(issueMsg.Symbol, issueMsg.Name, issueMsg.MinUnitAlias, issueMsg.Decimal, sdk.NewIntWithDecimal(int64(issueMsg.InitialSupply), int(issueMsg.Decimal)), sdk.NewIntWithDecimal(int64(issueMsg.MaxSupply), int(issueMsg.Decimal)), issueMsg.Mintable, issueMsg.Owner)
+	ft := types.NewFungibleToken(issueMsg.Symbol, issueMsg.Name, issueMsg.MinUnitAlias, issueMsg.Decimal, issueMsg.InitialSupply, issueMsg.MaxSupply, issueMsg.Mintable, issueMsg.Owner)
 	assert.NoError(t, err)
 
 	assert.Equal(t, ft, tokenSrc)
