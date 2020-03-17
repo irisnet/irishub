a60a8b4a1a0a32fb04f13a7c6f3588b706692302
secret <sufay30281@gmail.com>
2020-03-17 11:41:08 +0800

    modify upgrade

--- app/v3/coinswap/internal/keeper/upgrade.go
+++ app/v3/coinswap/internal/keeper/upgrade.go
@@ -10,50 +10,56 @@ import (
 
 func (k Keeper) Init(ctx sdk.Context, assetKeeper types.AssetKeeper, accountKeeper types.AccountKeeper) {
 	logger := k.Logger(ctx).With("handler", "Init")
+
 	tokens := assetKeeper.GetAllTokens(ctx)
 	logger.Info("Begin execute upgrade method")
+
 	for _, token := range tokens {
 		denom := token.GetDenom()
-		uniID, err := types.GetUniID(sdk.IrisAtto, denom)
+		voucherCoinName, err := types.GetVoucherCoinName(sdk.IrisAtto, denom)
 		if err == nil {
-			poolAcc, existed := k.getAccount(ctx, accountKeeper, uniID)
+			poolAcc, existed := k.getAccount(ctx, accountKeeper, voucherCoinName)
 			if !existed {
 				continue
 			}
 			balance := poolAcc.GetCoins()
-			uniDenom, _ := types.GetUniDenom(uniID)
+			voucherDenom, _ := types.GetVoucherDenom(voucherCoinName)
 
-			liquidity := balance.AmountOf(uniDenom)
+			liquidity := balance.AmountOf(voucherDenom)
 			if liquidity.LTE(sdk.ZeroInt()) {
 				continue
 			}
 
 			irisToken := sdk.NewCoin(sdk.IrisAtto, balance.AmountOf(sdk.IrisAtto))
 			otherToken := sdk.NewCoin(denom, balance.AmountOf(denom))
-			uniToken := sdk.NewCoin(uniDenom, liquidity)
+			voucherToken := sdk.NewCoin(voucherDenom, liquidity)
 
-			coins := sdk.NewCoins(irisToken, otherToken, uniToken)
-			//create pool for uniID
-			logger.Info("Create liquidity pool", "poolName", uniID)
-			_ = k.SetPool(ctx, types.NewPool(uniID, nil))
-			_ = k.SendCoinsFromAccountToPool(ctx, poolAcc.GetAddress(), uniID, coins)
+			coins := sdk.NewCoins(irisToken, otherToken, voucherToken)
+
+			//create pool for voucherCoinName
+			logger.Info("Create liquidity pool", "poolName", voucherCoinName)
+			_ = k.SetPool(ctx, types.NewPool(voucherCoinName, nil))
+			_ = k.SendCoinsFromAccountToPool(ctx, poolAcc.GetAddress(), voucherCoinName, coins)
 			logger.Info(
 				"Transfer coin to liquidity pool",
 				"from", poolAcc.GetAddress().String(),
 				"amount", coins.String(),
-				"poolName", uniID,
+				"poolName", voucherCoinName,
 			)
 		}
 	}
+
 	logger.Info("End execute upgrade method")
 }
 
 //Except for the upgrade process from v2 to v3, please do not use this code
-func (k Keeper) getAccount(ctx sdk.Context, accountKeeper types.AccountKeeper, uniId string) (account auth.Account, existed bool) {
-	swapPoolAccAddr := sdk.AccAddress(crypto.AddressHash([]byte(uniId)))
+func (k Keeper) getAccount(ctx sdk.Context, accountKeeper types.AccountKeeper, voucherCoinName string) (account auth.Account, existed bool) {
+	swapPoolAccAddr := sdk.AccAddress(crypto.AddressHash([]byte(voucherCoinName)))
+
 	account = accountKeeper.GetAccount(ctx, swapPoolAccAddr)
 	if account == nil {
 		return account, false
 	}
+
 	return account, true
 }
