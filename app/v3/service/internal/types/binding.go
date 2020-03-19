baac347c7c221f5e3f725c80a8a196bb2c2bf024
secret <sufay30281@gmail.com>
2020-03-19 17:59:39 +0800

    add price handling

--- app/v3/service/internal/types/binding.go
+++ app/v3/service/internal/types/binding.go
@@ -79,13 +79,13 @@ type Pricing struct {
 type PromotionByTime struct {
 	StartTime time.Time `json:"start_time"` // starting time of the promotion
 	EndTime   time.Time `json:"end_time"`   // ending time of the promotion
-	Discount  float32   `json:"discount"`   // discount during the promotion
+	Discount  sdk.Dec   `json:"discount"`   // discount during the promotion
 }
 
 // PromotionByVolume defines the promotion activity by volume
 type PromotionByVolume struct {
 	Volume   uint64  `json:"volume"`   // minimal volume for the promotion
-	Discount float32 `json:"discount"` // discount for the promotion
+	Discount sdk.Dec `json:"discount"` // discount for the promotion
 }
 
 // ParsePricing parses the given pricing string
@@ -96,6 +96,55 @@ func ParsePricing(pricing string) (Pricing, sdk.Error) {
 	}
 
 	return p, nil
+}
+
+// GetPrice gets the current price by the specified volume and time
+// Note: ensure that the pricing is valid
+func GetPrice(pricing string, time time.Time, volume uint64) sdk.Coins {
+	p, _ := ParsePricing(pricing)
+
+	// get discounts
+	discountByTime := GetDiscountByTime(p.PromotionsByTime, time)
+	discountByVolume := GetDiscountByVolume(p.PromotionsByVolume, volume)
+
+	// compute the price
+	basePrice := p.Price.AmountOf(sdk.IrisAtto)
+	price := sdk.NewDecFromInt(basePrice).Mul(discountByTime).Mul(discountByVolume)
+
+	return sdk.NewCoins(sdk.NewCoin(sdk.IrisAtto, price.TruncateInt()))
+}
+
+// GetDiscountByTime gets the current discount level by the specified time
+func GetDiscountByTime(promotionsByTime []PromotionByTime, time time.Time) sdk.Dec {
+	for _, p := range promotionsByTime {
+		if (time.Equal(p.StartTime) || time.After(p.StartTime)) &&
+			(time.Equal(p.EndTime) || time.Before(p.EndTime)) {
+			return p.Discount
+		}
+	}
+
+	return sdk.OneDec()
+}
+
+// GetDiscountByVolume gets the current discount level by the specified volume
+func GetDiscountByVolume(promotionsByVolume []PromotionByVolume, volume uint64) sdk.Dec {
+	if len(promotionsByVolume) > 0 {
+		if volume < promotionsByVolume[0].Volume {
+			return sdk.OneDec()
+		}
+
+		if volume >= promotionsByVolume[len(promotionsByVolume)-1].Volume {
+			return promotionsByVolume[len(promotionsByVolume)-1].Discount
+		}
+	}
+
+	for i, p := range promotionsByVolume {
+		if volume < p.Volume {
+			return promotionsByVolume[i-1].Discount
+		}
+	}
+
+	return sdk.OneDec()
 }
 
 func (binding ServiceBinding) Validate() sdk.Error {
