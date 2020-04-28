d657730ef3856f378287bc38ca71a9880dd8ab00
secret <sufay30281@gmail.com>
2020-04-28 10:01:48 +0800

    modify genesis

--- app/v3/service/genesis.go
+++ app/v3/service/genesis.go
@@ -25,9 +25,9 @@ func InitGenesis(ctx sdk.Context, k Keeper, data GenesisState) {
 		k.SetServiceBinding(ctx, binding)
 	}
 
-	for providerAddressStr, withdrawAddress := range data.WithdrawAddresses {
-		providerAddress, _ := sdk.AccAddressFromBech32(providerAddressStr)
-		k.SetWithdrawAddress(ctx, providerAddress, withdrawAddress)
+	for ownerAddressStr, withdrawAddress := range data.WithdrawAddresses {
+		ownerAddress, _ := sdk.AccAddressFromBech32(ownerAddressStr)
+		k.SetWithdrawAddress(ctx, ownerAddress, withdrawAddress)
 	}
 
 	for reqContextIDStr, requestContext := range data.RequestContexts {
@@ -61,8 +61,8 @@ func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
 
 	k.IterateWithdrawAddresses(
 		ctx,
-		func(providerAddress sdk.AccAddress, withdrawAddress sdk.AccAddress) bool {
-			withdrawAddresses[providerAddress.String()] = withdrawAddress
+		func(ownerAddress sdk.AccAddress, withdrawAddress sdk.AccAddress) bool {
+			withdrawAddresses[ownerAddress.String()] = withdrawAddress
 			return false
 		},
 	)
