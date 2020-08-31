47dd8341a767c3cd869adbd441c01975716f27d0
chengwenxi <vincent.ch.cn@gmail.com>
2020-08-31 17:36:03 +0800

    clean up

--- app/app.go
+++ app/app.go
@@ -512,6 +512,14 @@ func (app *IrisApp) EndBlocker(ctx sdk.Context, req abci.RequestEndBlock) abci.R
 func (app *IrisApp) InitChainer(ctx sdk.Context, req abci.RequestInitChain) abci.ResponseInitChain {
 	var genesisState GenesisState
 	app.cdc.MustUnmarshalJSON(req.AppStateBytes, &genesisState)
+
+	// add system service at InitChainer, overwrite if it exists
+	var serviceGenState servicetypes.GenesisState
+	app.appCodec.MustUnmarshalJSON(genesisState[servicetypes.ModuleName], &serviceGenState)
+	serviceGenState.Definitions = append(serviceGenState.Definitions, randomtypes.GetSvcDefinitions()...)
+
+	genesisState[servicetypes.ModuleName] = app.appCodec.MustMarshalJSON(&serviceGenState)
+
 	return app.mm.InitGenesis(ctx, app.appCodec, genesisState)
 }
 
--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/irismod/nft v1.1.1-0.20200827095318-d16861212579
 	github.com/irismod/record v1.1.1-0.20200827095301-3e27fc43ae73
 	github.com/irismod/service v1.1.1-0.20200830041912-b2ced48a06ab
-	github.com/irismod/token v1.1.1-0.20200831030503-21db6eea21a9
+	github.com/irismod/token v1.1.1-0.20200831092317-7be7d71fe448
 	github.com/olebedev/config v0.0.0-20190528211619-364964f3a8e4
 	github.com/onsi/ginkgo v1.8.0 // indirect
 	github.com/onsi/gomega v1.5.0 // indirect
