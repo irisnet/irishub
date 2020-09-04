e678a9f87cdddaa53eda20f674ccacdb36598732
bianjie <secret@bianjie.ai>
2020-09-04 16:27:11 +0800

    add grpc gateway for mint

--- modules/mint/module.go
+++ modules/mint/module.go
@@ -1,6 +1,7 @@
 package mint
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"math/rand"
@@ -66,6 +67,7 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCRoutes registers the gRPC Gateway routes for the mint module.
 func (a AppModuleBasic) RegisterGRPCRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
+	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
 }
 
 // GetTxCmd returns the root tx command for the mint module.
