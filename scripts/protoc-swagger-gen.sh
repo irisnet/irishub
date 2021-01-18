2cecb7f90c190675da8b67d3b550c41491820d44
chengwenxi <vincent.ch.cn@gmail.com>
2021-01-18 18:49:54 +0800

    update irismod version

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 SDK_VERSION=v0.40.0
-IRISMOD_VERSION=v1.1.1-0.20210114075305-def9678a8703
+IRISMOD_VERSION=v1.2.0
 
 chmod -R 755 ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/proto/cosmos
 chmod -R 755 ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/proto/ibc
