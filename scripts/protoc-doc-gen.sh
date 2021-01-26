c466bfeb3c9ac463733dec650929db862cc5006d
Segue <huoda.china@163.com>
2021-01-26 12:08:48 +0800

    improve script

--- scripts/protoc-doc-gen.sh
+++ scripts/protoc-doc-gen.sh
@@ -5,20 +5,16 @@ set -eo pipefail
 SDK_VERSION=v0.40.1
 IRISMOD_VERSION=v1.2.1-0.20210125095922-efbed3ccefba
 
-chmod -R 755 ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/proto/cosmos
-chmod -R 755 ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/proto/ibc
+chmod -R 755 ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/proto
 chmod -R 755 ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/third_party/proto
 chmod -R 755 ${GOPATH}/pkg/mod/github.com/irisnet/irismod@${IRISMOD_VERSION}/proto
 
 rm -rf tmp && mkdir -p tmp/proto tmp/third_party
 
-cp -r ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/proto/cosmos ./tmp/proto
-cp -r ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/proto/ibc ./tmp/proto
+cp -r ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/proto ./tmp && rm -rf ./tmp/proto/cosmos/mint
 cp -r ${GOPATH}/pkg/mod/github.com/cosmos/cosmos-sdk@${SDK_VERSION}/third_party/proto ./tmp/third_party
 cp -r ${GOPATH}/pkg/mod/github.com/irisnet/irismod@${IRISMOD_VERSION}/proto ./tmp
 cp -r ./proto ./tmp
-
-rm -rf ./tmp/proto/cosmos/mint
 
 # command to generate docs using protoc-doc-gen
 buf protoc \
