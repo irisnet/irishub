e8180ab57f12c672ec0b68fa084a1b80369060f0
张志强 <zhiqiang@bianjie.ai>
2021-02-04 14:10:02 +0800

    add ante for service

--- app/decorators.go
+++ app/decorators.go
@@ -8,7 +8,9 @@ import (
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	ibctransfertypes "github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
 
+	guardiankeeper "github.com/irisnet/irishub/modules/guardian/keeper"
 	coinswaptypes "github.com/irisnet/irismod/modules/coinswap/types"
+	servicetypes "github.com/irisnet/irismod/modules/service/types"
 	tokenkeeper "github.com/irisnet/irismod/modules/token/keeper"
 	tokentypes "github.com/irisnet/irismod/modules/token/types"
 )
@@ -48,6 +50,41 @@ func (ctd CheckTokenDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate b
 			if containSwapCoin(msg.Amount...) {
 				return ctx, sdkerrors.Wrap(
 					sdkerrors.ErrInvalidRequest, "can't deposit coinswap liquidity token for proposal")
+			}
+		}
+	}
+	return next(ctx, tx, simulate)
+}
+
+// ServiceAuthDecorator is responsible for checking the permission to execute MsgCallService
+type ServiceAuthDecorator struct {
+	gk guardiankeeper.Keeper
+}
+
+// NewServiceAuthDecorator return a instance of ServiceAuthDecorator
+func NewServiceAuthDecorator(gk guardiankeeper.Keeper) ServiceAuthDecorator {
+	return ServiceAuthDecorator{
+		gk: gk,
+	}
+}
+
+// AnteHandle check the transaction
+func (sad ServiceAuthDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+	for _, msg := range tx.GetMsgs() {
+		switch msg := msg.(type) {
+		case *servicetypes.MsgCallService:
+			if !msg.Repeated {
+				continue
+			}
+
+			consumer, err := sdk.AccAddressFromBech32(msg.Consumer)
+			if err != nil {
+				return ctx, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "invalid consumer")
+			}
+
+			if !sad.gk.Authorized(ctx, consumer) {
+				return ctx, sdkerrors.Wrap(
+					sdkerrors.ErrInvalidRequest, "authentication failed, only super accounts can create repeated service invocation")
 			}
 		}
 	}
