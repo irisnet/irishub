5aa7ceeaa22c3e75faaee3d25b8c3284a899d9b7
Segue <huoda.china@163.com>
2021-03-23 19:15:39 +0800

    fix migrate

--- migrate/service/migrate.go
+++ migrate/service/migrate.go
@@ -17,8 +17,19 @@ const (
 
 func Migrate(ctx sdk.Context, k servicekeeper.Keeper, bk bankkeeper.Keeper) error {
 	oldAcc := sdk.AccAddress(crypto.AddressHash([]byte(TaxAccName)))
-	params := k.GetParams(ctx)
-	params.RestrictedServiceFeeDenom = false
+	params := servicetypes.NewParams(
+		k.MaxRequestTimeout(ctx),
+		k.MinDepositMultiple(ctx),
+		k.MinDeposit(ctx),
+		k.ServiceFeeTax(ctx),
+		k.SlashFraction(ctx),
+		k.ComplaintRetrospect(ctx),
+		k.ArbitrationTimeLimit(ctx),
+		k.TxSizeLimit(ctx),
+		k.BaseDenom(ctx),
+		false,
+	)
+
 	k.SetParams(ctx, params)
 	return bk.SendCoinsFromAccountToModule(ctx, oldAcc, servicetypes.FeeCollectorName, bk.GetAllBalances(ctx, oldAcc))
 }
