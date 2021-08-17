0ec7fcfc46e460845a524f27e51c9bf4e392a5a0
chengwenxi <vincent.ch.cn@gmail.com>
2021-08-17 17:19:22 +0800

    fix cosmos-sdk v0.42.9 upgrade

--- app/app.go
+++ app/app.go
@@ -531,7 +531,7 @@ func NewIrisApp(
 	// CanWithdrawInvariant invariant.
 	// NOTE: staking module is required if HistoricalEntries param > 0
 	app.mm.SetOrderBeginBlockers(
-		upgradetypes.ModuleName, minttypes.ModuleName, distrtypes.ModuleName,
+		upgradetypes.ModuleName, capabilitytypes.ModuleName, minttypes.ModuleName, distrtypes.ModuleName,
 		slashingtypes.ModuleName, evidencetypes.ModuleName, stakingtypes.ModuleName,
 		ibchost.ModuleName, htlctypes.ModuleName, randomtypes.ModuleName, farmtypes.ModuleName,
 	)
