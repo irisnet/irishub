a908574da7fc86efe864a7dfb56a72971310be7c
mindcarver <32150062+mindcarver@users.noreply.github.com>
2021-10-11 21:58:09 +0800

    fix irishub load tibcnftTransfer bug (#2675)

--- app/app.go
+++ app/app.go
@@ -447,7 +447,7 @@ func NewIrisApp(
 	)
 	app.nftTransferKeeper = tibcnfttransferkeeper.NewKeeper(
 		appCodec, keys[tibcnfttypes.StoreKey], app.GetSubspace(tibcnfttypes.ModuleName),
-		app.accountKeeper, app.nftKeeper,
+		app.accountKeeper, &app.nftKeeper,
 		app.tibcKeeper.PacketKeeper, app.tibcKeeper.ClientKeeper,
 	)
 	// Create Transfer Keepers
