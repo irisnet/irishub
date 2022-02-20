be2237c9aa3d12e3053cbd468865d89918c5ea33
zyl <452174203@qq.com>
2022-02-20 14:25:15 +0800

    ignore genesis validate

--- modules/mt/types/genesis.go
+++ modules/mt/types/genesis.go
@@ -10,16 +10,17 @@ func NewGenesisState(collections []Collection) *GenesisState {
 // ValidateGenesis performs basic validation of mts genesis data returning an
 // error for any failed validation criteria.
 func ValidateGenesis(data GenesisState) error {
-	for _, c := range data.Collections {
-		if err := ValidateDenomID(c.Denom.Name); err != nil {
-			return err
-		}
-
-		for _, mt := range c.Mts {
-			if err := ValidateMTID(mt.GetID()); err != nil {
-				return err
-			}
-		}
-	}
+	// TODO
+	//for _, c := range data.Collections {
+	//	if err := ValidateDenomID(c.Denom.Name); err != nil {
+	//		return err
+	//	}
+	//
+	//	for _, mt := range c.Mts {
+	//		if err := ValidateMTID(mt.GetID()); err != nil {
+	//			return err
+	//		}
+	//	}
+	//}
 	return nil
 }
