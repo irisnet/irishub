83795f257378e531c2f7ce41f0cc78f8876e71c5
Dreamer <745124335@qq.com>
2023-04-03 15:04:35 +0800

    update comment

--- app/upgrades/v200/upgrades.go
+++ app/upgrades/v200/upgrades.go
@@ -49,7 +49,7 @@ func upgradeHandlerConstructor(m *module.Manager, c module.Configurator, app upg
 
 		tokenExp, ok := token.(*v1.Token)
 		if !ok {
-			return nil, fmt.Errorf("token is not v1.Token")
+			return nil, fmt.Errorf("token is not *v1.Token")
 		}
 		tokenExp.Owner = evmToken.Owner
 		if err := app.TokenKeeper.UpdateToken(ctx, *tokenExp); err != nil {
