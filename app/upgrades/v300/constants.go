2f6b730ad875531cbb08606bf643cb2a1ab0c059
Dreamer <zhiqiang2lucy@gmail.com>
2024-03-04 16:09:15 +0800

    update ValidatorLiquidStakingCap

--- app/upgrades/v300/constants.go
+++ app/upgrades/v300/constants.go
@@ -11,7 +11,7 @@ var (
 	ValidatorBondFactor = sdk.NewDec(250)
 	// ValidatorLiquidStakingCap represents a cap on the portion of stake that
 	// comes from liquid staking providers for a specific validator
-	ValidatorLiquidStakingCap = sdk.MustNewDecFromStr("0.5") // 50%
+	ValidatorLiquidStakingCap = sdk.MustNewDecFromStr("1") // 100%
 	// GlobalLiquidStakingCap represents the percentage cap on
 	// the portion of a chain's total stake can be liquid
 	GlobalLiquidStakingCap = sdk.MustNewDecFromStr("0.25") // 25%
