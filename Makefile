c40b52073c0ba1a5ff66ca3058ca2871f3dc3243
dreamer <dreamer.x.cn@gmail.com>
2024-06-25 13:50:14 +0800

    update .gitignore

--- Makefile
+++ Makefile
@@ -4,6 +4,3 @@ include scripts/build/contract.mk
 include scripts/build/protobuf.mk
 include scripts/build/testing.mk
 include scripts/build/linting.mk
-
-
-
---
+++ scripts/build/protobuf.mk
@@ -0,0 +1,19 @@
+protoVer=0.13.0
+protoImageName=ghcr.io/cosmos/proto-builder:$(protoVer)
+protoImage=$(DOCKER) run --rm -v $(CURDIR):/workspace --workdir /workspace $(protoImageName)
+
+proto-all: proto-format proto-lint proto-gen
+
+proto-gen:
+	@echo "Generating Protobuf files"
+	@$(protoImage) sh ./scripts/protocgen.sh
+
+proto-swagger-gen:
+	@echo "Generating Protobuf Swagger"
+	@$(protoImage) sh ./scripts/protoc-swagger-gen.sh
+
+proto-format:
+	@$(protoImage) find ./ -name "*.proto" -exec clang-format -i {} \;
+
+proto-lint:
+	@$(protoImage) buf lint --error-format=json
